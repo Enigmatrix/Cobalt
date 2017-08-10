9a0a550201717346e6fc0ec883f6ad88ba450a66
Enigmatrix <enigmatrix2000@gmail.com>
2017-08-10 21:50:57 +0800

    add invisible form that engine uses

---
+++ Cobalt.Engine/InvisibleForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cobalt.Engine
+{
+    public partial class InvisibleForm : Form
+    {
+        public InvisibleForm()
+        {
+            InitializeComponent();
+
+            FormBorderStyle = FormBorderStyle.None;
+            ShowInTaskbar = false;
+            Load += (o, e) => Size = new Size(0,0);
+        }
+    }
+}
