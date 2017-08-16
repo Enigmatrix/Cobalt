a9d2b7b60d64fc5722badd4a696cb6761a75447c
Enigmatrix <enigmatrix2000@gmail.com>
2017-08-16 08:32:53 +0800

    add setup projects

---
+++ Cobalt.Common.Analysis/AppStatsService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cobalt.Common.Analysis
+{
+    public interface IAppStatsService
+    {
+        
+    }
+
+    public class AppStatsService : IAppStatsService
+    {
+    }
+}
