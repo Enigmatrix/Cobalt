4dec35dec2a2cce2427212cc9faf9e229fbfd683
Enigmatrix <enigmatrix2000@gmail.com>
2017-09-01 18:03:12 +0800

    proper taskbarnotifier install

--- Cobalt.Setup.TaskScheduler/CustomAction.cs
+++ Cobalt.Setup.TaskScheduler/CustomAction.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Diagnostics;
 using System.IO;
-using System.Security.Principal;
 using Microsoft.Deployment.WindowsInstaller;
 using Microsoft.Win32.TaskScheduler;
 
@@ -13,55 +12,16 @@ namespace Cobalt.Setup.TaskScheduler
         public static ActionResult InstallCobaltEngineToTaskScheduler(Session session)
         {
             Log("start");
-            
+
             try
             {
                 var installLocation = session.CustomActionData["INSTALLFOLDER"];
-                
+
                 using (var ts = new TaskService())
                 {
-                    var task = ts.NewTask();
-
-                    task.Triggers.Add(new LogonTrigger());
-                    task.Actions.Add(
-                        new ExecAction($"{installLocation}Cobalt.Engine.exe", "", installLocation));
-                    task.Actions.Add(
-                        new ExecAction($"{installLocation}Cobalt.TaskbarNotifier.exe", "", installLocation));
-
-                    task.Principal.RunLevel = TaskRunLevel.Highest;
-                    task.Principal.LogonType = TaskLogonType.InteractiveToken;
-                    //users group
-                    task.Principal.GroupId = "S-1-5-32-545";
-
-                    task.Settings.MultipleInstances = TaskInstancesPolicy.IgnoreNew;
-                    task.Settings.DisallowStartIfOnBatteries = false;
-                    task.Settings.StopIfGoingOnBatteries = false;
-                    task.Settings.AllowHardTerminate = false;
-                    task.Settings.StartWhenAvailable = true;
-                    task.Settings.RunOnlyIfNetworkAvailable = false;
-                    task.Settings.IdleSettings.StopOnIdleEnd = false;
-                    task.Settings.IdleSettings.RestartOnIdle = false;
-                    task.Settings.AllowDemandStart = true;
-                    task.Settings.Hidden = false;
-                    task.Settings.Enabled = true;
-                    task.Settings.RunOnlyIfIdle = false;
-                    task.Settings.WakeToRun = false;
-                    task.Settings.Priority = ProcessPriorityClass.Normal;
-
-                    ts.RootFolder.RegisterTaskDefinition("Cobalt", task);
+                    Setup(installLocation, "Cobalt.Engine", ts);
+                    Setup(installLocation, "Cobalt.TaskbarNotifier", ts);
                 }
-                /*
-                Process.Start(new ProcessStartInfo
-                {
-
-                    UseShellExecute = false,
-                    FileName = "SCHTASKS.exe",
-                    RedirectStandardError = true,
-                    RedirectStandardOutput = true,
-                    CreateNoWindow = true,
-                    WindowStyle = ProcessWindowStyle.Hidden,
-                    Arguments = "/create /SC ONLOGON /TN Cobalt.Setup2 /RL HIGHEST /TR C:\\Windows\\System32\\calc.exe"
-                })?.WaitForExit();*/
             }
             catch (Exception e)
             {
@@ -72,9 +32,40 @@ namespace Cobalt.Setup.TaskScheduler
             return ActionResult.Success;
         }
 
+        private static void Setup(string installLocation, string prog, TaskService ts)
+        {
+            var task = ts.NewTask();
+
+            task.Triggers.Add(new LogonTrigger());
+            task.Actions.Add(
+                new ExecAction($"{installLocation}{prog}.exe", "", installLocation));
+
+            task.Principal.RunLevel = TaskRunLevel.Highest;
+            task.Principal.LogonType = TaskLogonType.InteractiveToken;
+            //users group
+            task.Principal.GroupId = "S-1-5-32-545";
+
+            task.Settings.MultipleInstances = TaskInstancesPolicy.IgnoreNew;
+            task.Settings.DisallowStartIfOnBatteries = false;
+            task.Settings.StopIfGoingOnBatteries = false;
+            task.Settings.AllowHardTerminate = false;
+            task.Settings.StartWhenAvailable = true;
+            task.Settings.RunOnlyIfNetworkAvailable = false;
+            task.Settings.IdleSettings.StopOnIdleEnd = false;
+            task.Settings.IdleSettings.RestartOnIdle = false;
+            task.Settings.AllowDemandStart = true;
+            task.Settings.Hidden = false;
+            task.Settings.Enabled = true;
+            task.Settings.RunOnlyIfIdle = false;
+            task.Settings.WakeToRun = false;
+            task.Settings.Priority = ProcessPriorityClass.Normal;
+
+            ts.RootFolder.RegisterTaskDefinition(prog, task);
+        }
+
         private static void Log(string msg)
         {
-            File.AppendAllText(@"C:\Users\enigm\wixlog.txt", msg+"\r\n");
+            File.AppendAllText(@"C:\Users\enigm\wixlog.txt", msg + "\r\n");
         }
     }
 }
