f20fcc21e1c0e159936e4351dff7d70ad1e5eb1b
Enigmatrix <enigmatrix2000@gmail.com>
2017-09-05 17:50:28 +0800

    bolding when selected (taskbarnotifier's pie)

--- Cobalt.TaskbarNotifier/AppDurationTooltip.xaml.cs
+++ Cobalt.TaskbarNotifier/AppDurationTooltip.xaml.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using Cobalt.Common.UI.ViewModels;
 using LiveCharts;
 using LiveCharts.Wpf;
 
@@ -14,6 +15,7 @@ namespace Cobalt.TaskbarNotifier
     {
         private TooltipData _data;
         private List<DataPointViewModel> _sortedPoints;
+        private string _selectedPath;
 
         public AppDurationTooltip()
         {
@@ -24,11 +26,7 @@ namespace Cobalt.TaskbarNotifier
         public List<DataPointViewModel> SortedPoints
         {
             get => _sortedPoints;
-            set
-            {
-                _sortedPoints = value;
-                OnPropertyChanged();
-            }
+            set => Set(ref _sortedPoints, value);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -38,16 +36,23 @@ namespace Cobalt.TaskbarNotifier
             get => _data;
             set
             {
-                _data = value;
-                OnPropertyChanged();
+                Set(ref _data, value);
                 SortedPoints = _data.Points.OrderByDescending(x => x.ChartPoint.Participation).ToList();
+                SelectedPath = ((IAppDurationViewModel) _data.SenderSeries.ChartPoints.Single().Instance).App.Path;
             }
+        }
+
+        public string SelectedPath
+        {
+            get => _selectedPath;
+            set => Set(ref _selectedPath, value);
         }
 
         public TooltipSelectionMode? SelectionMode { get; set; }
 
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        protected virtual void Set<T>(ref T m, T val, [CallerMemberName] string propertyName = null)
         {
+            m = val;
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
