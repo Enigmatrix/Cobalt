87dfca505f12026fe9503b0f1f002d79fc1a7324
Enigmatrix <enigmatrix2000@gmail.com>
2018-01-25 14:37:58 +0800

    punish changes after this

---
+++ Cobalt/Views/Controls/DateRangePicker.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cobalt.Views.Controls
+{
+    /// <summary>
+    /// Interaction logic for DateRangePicker.xaml
+    /// </summary>
+    public partial class DateRangePicker
+    {
+        public DateRangePicker()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public DateTime Start
+        {
+            get => (DateTime)GetValue(StartProperty);
+            set => SetValue(StartProperty, value);
+        }
+
+        public static readonly DependencyProperty StartProperty =
+            DependencyProperty.Register("Start", typeof(DateTime), typeof(DateRangePicker), new PropertyMetadata(DateTime.Today.Subtract(TimeSpan.FromDays(1))));
+
+
+        public DateTime End
+        {
+            get => (DateTime)GetValue(EndProperty);
+            set => SetValue(EndProperty, value);
+        }
+
+        public static readonly DependencyProperty EndProperty =
+            DependencyProperty.Register("End", typeof(DateTime), typeof(DateRangePicker), new PropertyMetadata(DateTime.Today));
+
+
+    }
+}
---
+++ Cobalt/Views/Util/ContentDataTemplateSelector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Cobalt.Views.Util
+{
+    public class ContentDataTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
