3f56e532fac3c5d608d65642a6a985b5e0310bc4
Enigmatrix <enigmatrix2000@gmail.com>
2018-01-27 16:42:44 +0800

    small update

--- Cobalt/Views/Controls/DateRangePicker.xaml.cs
+++ Cobalt/Views/Controls/DateRangePicker.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace Cobalt.Views.Controls
 {
@@ -26,26 +27,65 @@ namespace Cobalt.Views.Controls
         }
 
 
+        public static readonly DependencyProperty StartSetProperty =
+            DependencyProperty.RegisterAttached("StartSet", typeof(DateTime?), typeof(DateTimePicker),
+                new PropertyMetadata(DateTime.Now, PropertyChangedCallback));
 
-        public DateTime Start
+        public static bool GetStartSet(DependencyObject obj)
         {
-            get => (DateTime)GetValue(StartProperty);
+            return (bool) obj.GetValue(StartSetProperty);
+        }
+
+        public static void SetStartSet(DependencyObject obj, DateTime? value)
+        {
+            obj.SetValue(StartSetProperty, value);
+        }
+
+        public static readonly DependencyProperty EndSetProperty =
+            DependencyProperty.RegisterAttached("EndSet", typeof(DateTime?), typeof(DateTimePicker),
+                new PropertyMetadata(DateTime.Now, PropertyChangedCallback));
+
+        public static bool GetEndSet(DependencyObject obj)
+        {
+            return (bool) obj.GetValue(EndSetProperty);
+        }
+
+        public static void SetEndSet(DependencyObject obj, DateTime? value)
+        {
+            obj.SetValue(EndSetProperty, value);
+        }
+
+        private static void PropertyChangedCallback(DependencyObject dependencyObject,
+            DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+        }
+
+        public DateTime? Start
+        {
+            get => (DateTime?)GetValue(StartProperty);
             set => SetValue(StartProperty, value);
         }
 
         public static readonly DependencyProperty StartProperty =
-            DependencyProperty.Register("Start", typeof(DateTime), typeof(DateRangePicker), new PropertyMetadata(DateTime.Today.Subtract(TimeSpan.FromDays(1))));
+            DependencyProperty.Register("Start", typeof(DateTime?), typeof(DateRangePicker), new PropertyMetadata(DateTime.Today.Subtract(TimeSpan.FromDays(1))));
 
 
-        public DateTime End
+        public DateTime? End
         {
-            get => (DateTime)GetValue(EndProperty);
+            get => (DateTime?)GetValue(EndProperty);
             set => SetValue(EndProperty, value);
         }
 
         public static readonly DependencyProperty EndProperty =
-            DependencyProperty.Register("End", typeof(DateTime), typeof(DateRangePicker), new PropertyMetadata(DateTime.Today));
+            DependencyProperty.Register("End", typeof(DateTime?), typeof(DateRangePicker), new PropertyMetadata(DateTime.Today));
 
+
+        public DateTime? YesterdayStart { get; } = DateTime.Today.Subtract(TimeSpan.FromDays(1));
+        public DateTime? YesterdayEnd { get; } = DateTime.Today;
+        
+
+        public DateTime? Last7DaysStart { get; } = DateTime.Today.Subtract(TimeSpan.FromDays(6));
+        public DateTime? Last7DaysEnd { get; } = null;
 
     }
 }
