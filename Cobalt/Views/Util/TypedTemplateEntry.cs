39319b6dbedb2379c941f5c90938651907c0073d
Enigmatrix <enigmatrix2000@gmail.com>
2018-01-29 11:48:23 +0800

    better text formatting

---
+++ Cobalt/Views/Util/TypedTemplateEntry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Cobalt.Views.Util
+{
+    public class TypedTemplateEntry
+    {
+        public Type Type { get; set; }
+        public DataTemplate Template { get; set; }
+    }
+
+    public class TypedTemplateEntries : Collection<TypedTemplateEntry>
+    {
+
+    }
+}
