f1f362e794f7ac3488f8ce42ff2db9e4a7eaad97
Enigmatrix <enigmatrix2000@gmail.com>
2018-02-03 11:46:40 +0800

    semi=working expand for datacard

--- Cobalt/Views/Controls/DataCard.xaml.cs
+++ Cobalt/Views/Controls/DataCard.xaml.cs
@@ -1,4 +1,11 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using LiveCharts;
+using LiveCharts.Wpf;
+using LiveCharts.Wpf.Charts.Base;
+using MaterialDesignThemes.Wpf;
 
 namespace Cobalt.Views.Controls
 {
@@ -12,9 +19,32 @@ namespace Cobalt.Views.Controls
             InitializeComponent();
         }
 
-        private void DataCardExpand(object sender, RoutedEventArgs e)
+        private async void DataCardExpand(object sender, RoutedEventArgs e)
         {
+            var container = (ContentControl) FindResource("DialogContainer");
+            var root = (Grid) GetTemplateChild("Root");
+            var contentHolder = (ContentPresenter) GetTemplateChild("ContentHolder");
+            var parent = (ContentControl) root.Parent;
 
+            parent.Content = null;
+            container.Content = root;
+
+            await this.ShowDialog(container, (o, oe) =>
+            {
+                container.UpdateLayout();
+                if (contentHolder.Content is Chart chart)
+                {
+                    chart.Update(true, true);
+
+                }
+            }, (o, ce) =>
+            {
+                container.Content = null;
+                parent.Content = root;
+                root.UpdateLayout();
+                if (contentHolder.Content is Chart chart)
+                    chart.Update(true, true);
+            });
         }
     }
 }
