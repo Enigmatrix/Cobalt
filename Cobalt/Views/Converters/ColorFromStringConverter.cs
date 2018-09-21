7247da776a810f935f029097d1956d0a557d556a
Enigmatrix <enigmatrix2000@gmail.com>
2018-09-21 22:04:14 +0800

    simple colorpicker

---
+++ Cobalt/Views/Converters/ColorFromStringConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Cobalt.Views.Converters
+{
+    public class ColorFromStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                return ColorConverter.ConvertFromString((string) value);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var color = (Color) value;
+            return $"#{color.A:X2}{color.R:X2}{color.G:X2}{color.B:X2}";
+        }
+    }
+}
