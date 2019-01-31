d851848344aa6c485d2f6ceed1fe4407d4981125
Enigmatrix <enigmatrix2000@gmail.com>
2019-01-31 17:01:38 +0800

    yoinks

--- Cobalt.TaskbarNotifier/App.xaml.cs
+++ Cobalt.TaskbarNotifier/App.xaml.cs
@@ -5,7 +5,7 @@ namespace Cobalt.TaskbarNotifier
     /// <summary>
     ///     Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
     }
 }
