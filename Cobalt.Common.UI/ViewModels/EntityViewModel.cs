d4d7da172a4e952288005465de3984fc950eb15d
Enigmatrix <enigmatrix2000@gmail.com>
2019-02-23 13:07:27 +0800

    :)

---
+++ Cobalt.Common.UI/ViewModels/EntityViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Cobalt.Common.Data.Entities;
+
+namespace Cobalt.Common.UI.ViewModels
+{
+    public abstract class EntityViewModel : ViewModelBase
+    {
+        public Entity Entity { get; }
+
+        protected EntityViewModel(Entity entity)
+        {
+            Entity = entity;
+        }
+
+        public long Id => Entity.Id;
+    }
+
+    public abstract class EntityViewModel<T> : EntityViewModel
+        where T : Entity
+    {
+        protected EntityViewModel(T entity) : base(entity)
+        {
+        }
+
+        public new T Entity => (T) base.Entity;
+    }
+}
---
+++ Cobalt.Common.UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Reactive.Disposables;
+using ReactiveUI;
+
+namespace Cobalt.Common.UI.ViewModels
+{
+    public abstract class ViewModelBase : ReactiveObject, ISupportsActivation
+    {
+        public ViewModelActivator Activator { get; }
+
+        public ViewModelBase()
+        {
+            Activator = new ViewModelActivator();
+            this.WhenActivated(() =>
+            {
+                var disp = new CompositeDisposable();
+                Activate(disp);
+                return disp;
+            });
+        }
+
+        protected virtual void Activate(CompositeDisposable disp)
+        {
+
+        }
+    }
+}
