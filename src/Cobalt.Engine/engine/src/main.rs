971d7abbf51b93dd3407278eb8b5d3c5e5ef4692
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2023-05-05 02:00:50 +0800

    feat(engine): first log

--- src/Cobalt.Engine/engine/src/main.rs
+++ src/Cobalt.Engine/engine/src/main.rs
@@ -1,9 +1,11 @@
 use common::errors::*;
 use common::settings::Settings;
+use common::tracing::*;
 
 fn main() -> Result<()> {
     let settings = Settings::from_file("appsettings.json").context("fetch settings")?;
     common::setup(&settings).context("setup common")?;
+    info!("engine is running");
 
     Ok(())
 }
