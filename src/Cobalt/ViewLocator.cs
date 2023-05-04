461a5bea5c02b76aeee3321e0b366170d881be7e
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2023-05-04 21:14:09 +0800

    feat(ui): install FluentAvalonia

--- src/Cobalt/ViewLocator.cs
+++ src/Cobalt/ViewLocator.cs
@@ -7,8 +7,11 @@ namespace Cobalt
 {
     public class ViewLocator : IDataTemplate
     {
-        public IControl Build(object data)
+        public IControl Build(object? data)
         {
+            if (data is null)
+                return new TextBlock { Text = "Null DataContext" };
+
             var name = data.GetType().FullName!.Replace("ViewModel", "View");
             var type = Type.GetType(name);
 
@@ -20,7 +23,7 @@ namespace Cobalt
             return new TextBlock { Text = "Not Found: " + name };
         }
 
-        public bool Match(object data)
+        public bool Match(object? data)
         {
             return data is ViewModelBase;
         }
