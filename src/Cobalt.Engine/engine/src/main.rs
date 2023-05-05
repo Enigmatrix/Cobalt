74370ee4b68c871c7483316fcbbb808243a8e974
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2023-05-05 15:39:35 +0800

    feat(engine): simple tester

--- src/Cobalt.Engine/engine/src/main.rs
+++ src/Cobalt.Engine/engine/src/main.rs
@@ -7,5 +7,17 @@ fn main() -> Result<()> {
     common::setup(&settings).context("setup common")?;
     info!("engine is running");
 
-    Ok(())
+    loop {
+        if let Some(window) = platform::objects::Window::foreground() {
+            let process = platform::objects::Process::new(window.pid()?)?;
+            let path = process.path()?;
+            info!(title = window.title()?);
+            info!(path = path);
+            if process.is_uwp(Some(&path))? {
+                info!(aumid = window.aumid()?);
+            }
+        }
+
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }
 }
