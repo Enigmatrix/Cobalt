11ea3c7de25a5340b35883f354e149325717732b
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2023-05-05 14:45:50 +0800

    feat(engine): buffer & window completion

--- src/Cobalt.Engine/platform/src/lib.rs
+++ src/Cobalt.Engine/platform/src/lib.rs
@@ -1,2 +1,10 @@
+#![feature(new_uninit)]
+#![feature(maybe_uninit_slice)]
+#![feature(maybe_uninit_uninit_array)]
+
+pub mod buffers;
 pub mod errors;
 pub mod objects;
+
+pub use buffers::*;
+pub use errors::*;
--- src/Cobalt.Engine/platform/src/objects/window.rs
+++ src/Cobalt.Engine/platform/src/objects/window.rs
@@ -1,9 +1,19 @@
+use std::ptr;
+
 use common::errors::*;
+use windows::core::{ComInterface, Interface};
+use windows::imp::CoTaskMemFree;
 use windows::Win32::Foundation::HWND;
-use windows::Win32::UI::WindowsAndMessaging::{GetForegroundWindow, GetWindowThreadProcessId};
+use windows::Win32::Storage::EnhancedStorage::PKEY_AppUserModel_ID;
+use windows::Win32::UI::Shell::PropertiesSystem::{
+    IPropertyStore, PropVariantToStringAlloc, SHGetPropertyStoreForWindow,
+};
+use windows::Win32::UI::WindowsAndMessaging::{
+    GetForegroundWindow, GetWindowTextLengthW, GetWindowTextW, GetWindowThreadProcessId,
+};
 
 use crate::objects::process::*;
-use crate::win32;
+use crate::*;
 
 /// Representation of a Window on the user's desktop
 pub struct Window {
@@ -26,11 +36,57 @@ impl Window {
         }
     }
 
-    /// Get [ProcessId] of [Window]
+    /// Get the [ProcessId] of the [Window]
     pub fn pid(&self) -> Result<ProcessId> {
         let mut pid = 0;
         win32!(val: unsafe { GetWindowThreadProcessId(self.inner, Some(&mut pid)) })
             .context("get window pid")?;
         Ok(pid)
+    }
+
+    /// Get the title of the [Window]
+    pub fn title(&self) -> Result<String> {
+        let len = win32!(val: unsafe { GetWindowTextLengthW(self.inner) })
+            .context("get window title length")?;
+        let title = if len == 0 {
+            String::new()
+        } else {
+            let mut buf = buffers::buf(len as usize);
+            let written = win32!(val: unsafe { GetWindowTextW(self.inner, buf.as_bytes()) })
+                .context("get window title")?;
+            buf.with_length(written as usize).to_string_lossy()
+        };
+        Ok(title)
+    }
+
+    /// Get the [AUMID](https://learn.microsoft.com/en-us/windows/win32/shell/appids) of the [Window]
+    pub fn aumid(&self) -> Result<String> {
+        let mut prop_store_raw = ptr::null_mut();
+        unsafe {
+            SHGetPropertyStoreForWindow(
+                self.inner,
+                &<IPropertyStore as ComInterface>::IID,
+                &mut prop_store_raw,
+            )
+            .context("get window property store")?;
+        };
+
+        let prop_store = unsafe { IPropertyStore::from_raw(prop_store_raw) };
+
+        let aumid_prop = unsafe {
+            prop_store
+                .GetValue(&PKEY_AppUserModel_ID)
+                .context("get window aumid property")?
+        };
+
+        let aumid_raw = unsafe {
+            PropVariantToStringAlloc(&aumid_prop)
+                .context("get aumid property as allocated string")?
+        };
+
+        let aumid = String::from_utf16_lossy(unsafe { aumid_raw.as_wide() });
+        unsafe { CoTaskMemFree(aumid_raw.as_ptr().cast()) };
+
+        Ok(aumid)
     }
 }
