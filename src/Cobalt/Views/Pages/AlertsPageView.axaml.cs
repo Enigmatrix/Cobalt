2cb00dc0a7b055b9cfa2d3895a0376f543d90336
Enigmatrix <enigmatrix2000@gmail.com>
2023-06-17 17:05:21 +0800

    feat(ui): just some testing pages

--- src/Cobalt/Views/Pages/AlertsPageView.axaml.cs
+++ src/Cobalt/Views/Pages/AlertsPageView.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Cobalt.Common.ViewModels.Dialogs;
+using Cobalt.Views.Dialogs;
 
 namespace Cobalt.Views.Pages;
 
@@ -7,5 +10,14 @@ public partial class AlertsPageView : UserControl
     public AlertsPageView()
     {
         InitializeComponent();
+    }
+
+    private async void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var dialog = new AddAlertDialogView
+        {
+            DataContext = new AddAlertDialogViewModel(null)
+        };
+        var result = await dialog.ShowAsync();
     }
 }
--- src/Cobalt/Views/Pages/AppsPageView.axaml.cs
+++ src/Cobalt/Views/Pages/AppsPageView.axaml.cs
@@ -1,4 +1,15 @@
+using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
 using Avalonia.Controls;
+using Cobalt.Common.Analysis;
+using Cobalt.Common.Analysis.Graphs;
+using Cobalt.Common.ViewModels.Entities;
+using LiveChartsCore;
+using LiveChartsCore.Kernel;
+using LiveChartsCore.Kernel.Sketches;
 
 namespace Cobalt.Views.Pages;
 
@@ -6,6 +17,54 @@ public partial class AppsPageView : UserControl
 {
     public AppsPageView()
     {
+        var stat = new Statistic<WithDuration<AppViewModel>>(
+            () => new[]
+            {
+                new WithDuration<AppViewModel>(new AppViewModel(null!, null!)
+                {
+                    Name = "Chrome",
+                    Color = "#89dceb",
+                    Icon = Fetch(
+                        "https://upload.wikimedia.org/wikipedia/commons/8/87/Google_Chrome_icon_%282011%29.png")
+                }, TimeSpan.FromHours(20)),
+                new WithDuration<AppViewModel>(new AppViewModel(null!, null!)
+                {
+                    Name = "League of Legends",
+                    Color = "#fab387",
+                    Icon = Fetch(
+                        "https://icon-library.com/images/league-of-legends-icon-transparent/league-of-legends-icon-transparent-0.jpg")
+                }, TimeSpan.FromHours(10)),
+                new WithDuration<AppViewModel>(new AppViewModel(null!, null!)
+                {
+                    Name = "Cobalt",
+                    Color = "#f2cdcd",
+                    Icon = Fetch("https://raw.githubusercontent.com/Enigmatrix/Cobalt/master/images/icon_512.png")
+                }, TimeSpan.FromHours(5))
+            }.AsQueryable(), new PieGraph<WithDuration<AppViewModel>>());
+
+        // TODO this needs to be sleeker
+        stat.Graph.SetData(stat.Query.Execute());
+        Series = stat.Graph.Series;
+
         InitializeComponent();
+    }
+
+    public ObservableCollection<ISeries> Series { get; set; } = new();
+
+    private Stream Fetch(string url)
+    {
+        var client = new HttpClient();
+        using var stream = client.GetStreamAsync(url).Result;
+        var mem = new MemoryStream();
+        stream.CopyTo(mem);
+        mem.Seek(0, SeekOrigin.Begin);
+        return mem;
+    }
+
+
+    private void PieChart_OnChartPointPointerDown(IChartView chart, ChartPoint? point)
+    {
+        if (point?.Context.DataSource is WithDuration<AppViewModel> withDur)
+            withDur.Duration += TimeSpan.FromHours(1);
     }
 }
