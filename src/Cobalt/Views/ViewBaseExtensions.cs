f4fbdf96c98bd1b9ef37a09272dfc058ef9ccaed
Enigmatrix <enigmatrix2000@gmail.com>
2023-06-19 21:47:51 +0800

    feat(ui): add WhenActivated extension, resolves #93

---
+++ src/Cobalt/Views/ViewBaseExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Avalonia.Controls;
+
+namespace Cobalt.Views;
+
+public static class ViewBaseExtensions
+{
+    public static void WhenActivated(this Control control, Func<Action> activation)
+    {
+        Action cleanup = default!;
+
+        void Loaded(object? _, EventArgs args)
+        {
+            cleanup = activation();
+        }
+
+        void Unloaded(object? _, EventArgs args)
+        {
+            cleanup();
+            control.Loaded -= Loaded;
+            control.Unloaded -= Unloaded;
+        }
+
+        control.Loaded += Loaded;
+        control.Unloaded += Unloaded;
+    }
+
+    /*
+    public static void WhenActivated(this Visual visual, Func<Action> activation)
+    {
+        Action cleanup = default!;
+        void Attached(object? _, EventArgs args)
+        {
+            cleanup = activation();
+        }
+
+        void Detached(object? _, EventArgs args)
+        {
+            cleanup();
+            visual.AttachedToVisualTree -= Attached;
+            visual.DetachedFromVisualTree -= Detached;
+        }
+
+        visual.AttachedToVisualTree += Attached;
+        visual.DetachedFromVisualTree += Detached;
+    }
+    */
+}
