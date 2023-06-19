136a294d82f98fd676ba420e0b108d07589d7fb3
Enigmatrix <enigmatrix2000@gmail.com>
2023-06-19 22:53:57 +0800

    feat(ui): add Interaction handlers, resolves #92

---
+++ src/Cobalt.Common.ViewModels/Interactions/Interaction.cs
@@ -0,0 +1,30 @@
+﻿namespace Cobalt.Common.ViewModels.Interactions;
+
+public class Interaction<TInput, TOutput>
+{
+    private readonly List<Func<InteractionContext<TInput, TOutput>, ValueTask>> _handlers;
+
+    public Interaction()
+    {
+        _handlers = new List<Func<InteractionContext<TInput, TOutput>, ValueTask>>();
+    }
+
+    public Action RegisterHandler(Func<InteractionContext<TInput, TOutput>, ValueTask> handler)
+    {
+        _handlers.Add(handler);
+        return () => _handlers.Remove(handler);
+    }
+
+    public async ValueTask<TOutput?> Handle(TInput input)
+    {
+        var ctx = new InteractionContext<TInput, TOutput>(input);
+        foreach (var handler in _handlers.AsEnumerable().Reverse())
+        {
+            await handler(ctx);
+            var output = ctx.GetOutput();
+            if (output != null) return output;
+        }
+
+        return default;
+    }
+}
---
+++ src/Cobalt.Common.ViewModels/Interactions/InteractionContext.cs
@@ -0,0 +1,23 @@
+﻿namespace Cobalt.Common.ViewModels.Interactions;
+
+public class InteractionContext<TInput, TOutput>
+{
+    private TOutput? _output;
+
+    public InteractionContext(TInput input)
+    {
+        Input = input;
+    }
+
+    public TInput Input { get; }
+
+    public void SetOutput(TOutput output)
+    {
+        _output = output;
+    }
+
+    public TOutput? GetOutput()
+    {
+        return _output;
+    }
+}
