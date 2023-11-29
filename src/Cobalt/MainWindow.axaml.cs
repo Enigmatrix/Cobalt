edafd81b97f980bba5bfb9c1b1df2ac749e3c755
Enigmatrix <enigmatrix2000@gmail.com>
2023-11-29 22:54:16 +0800

    feat(ui): init

---
+++ src/Cobalt/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Cobalt
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
