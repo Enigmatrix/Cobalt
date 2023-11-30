01db90e00bcae108017a33fd32432b0c1f912a4c
Enigmatrix <enigmatrix2000@gmail.com>
2023-11-30 17:17:43 +0800

    feat(data): _app_tags table convention

--- src/Cobalt.Common.Data/QueryContext.cs
+++ src/Cobalt.Common.Data/QueryContext.cs
@@ -11,7 +11,7 @@ public class QueryContext : DbContext
     }
 
     public DbSet<App> Apps { get; set; } = null!;
-    public DbSet<Session> Session { get; set; } = null!;
+    public DbSet<Session> Sessions { get; set; } = null!;
     public DbSet<Usage> Usages { get; set; } = null!;
     public DbSet<Tag> Tags { get; set; } = null!;
     public DbSet<InteractionPeriod> InteractionPeriods { get; set; } = null!;
@@ -37,5 +37,15 @@ public class QueryContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        // The many-to-many relation table should be called _app_tags.
+        // Leaving this out generates a table called app_tag
+        modelBuilder.Entity<App>()
+            .HasMany(e => e.Tags)
+            .WithMany(e => e.Apps)
+            .UsingEntity("_app_tags",
+                l => l.HasOne(typeof(Tag)).WithMany().HasForeignKey("tag_id").HasPrincipalKey(nameof(Tag.Id)),
+                r => r.HasOne(typeof(App)).WithMany().HasForeignKey("app_id").HasPrincipalKey(nameof(App.Id)),
+                j => j.HasKey("app_id", "tag_id")
+            );
     }
 }
