5f3c1d6b42c893c49ea5f267bf0325255dcca9a5
Enigmatrix <enigmatrix2000@gmail.com>
2023-11-30 18:42:52 +0800

    feat(vm): init

---
+++ src/Cobalt.Common.ViewModels/Entities/EntityViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Cobalt.Common.ViewModels.Entities;
+
+public abstract partial class EntityViewModelBase : ViewModelBase
+{
+    [ObservableProperty] private long _id;
+}
