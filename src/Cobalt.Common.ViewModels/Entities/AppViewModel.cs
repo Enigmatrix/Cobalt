bae05f67ec7c84b103083f5dd98244cc679662ce
Enigmatrix <enigmatrix2000@gmail.com>
2023-11-30 19:46:53 +0800

    feat(vm): initial viewmodels for App and Tag

---
+++ src/Cobalt.Common.ViewModels/Entities/AppViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Cobalt.Common.Data;
+using Cobalt.Common.Data.Entities;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cobalt.Common.ViewModels.Entities;
+
+public partial class AppViewModel : EditableEntityViewModelBase<App>
+{
+    [ObservableProperty] private string _color;
+    [ObservableProperty] private string _company;
+    [ObservableProperty] private string _description;
+    [ObservableProperty] private AppIdentity _identity;
+    [ObservableProperty] private bool _initialized;
+    [ObservableProperty] private string _name;
+
+    public AppViewModel(App entity, IDbContextFactory<QueryContext> contexts) : base(entity, contexts)
+    {
+        _initialized = entity.Initialized;
+        _name = entity.Name;
+        _description = entity.Description;
+        _company = entity.Company;
+        _color = entity.Color;
+        _identity = entity.Identity;
+    }
+
+    public override void UpdateEntity()
+    {
+        Entity.Initialized = true; // cannot be set to false as a user
+        Entity.Name = Name;
+        Entity.Description = Description;
+        Entity.Company = Company;
+        Entity.Color = Color;
+        Entity.Identity = Identity;
+    }
+}
---
+++ src/Cobalt.Common.ViewModels/Entities/TagViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Cobalt.Common.Data;
+using Cobalt.Common.Data.Entities;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cobalt.Common.ViewModels.Entities;
+
+public partial class TagViewModel : EditableEntityViewModelBase<Tag>
+{
+    [ObservableProperty] private string _color;
+    [ObservableProperty] private string _name;
+
+    public TagViewModel(Tag entity, IDbContextFactory<QueryContext> contexts) : base(entity, contexts)
+    {
+        _name = entity.Name;
+        _color = entity.Color;
+    }
+
+    public override void UpdateEntity()
+    {
+        Entity.Name = Name;
+        Entity.Color = Color;
+    }
+}
