0df8178c6ddfb7a0e0173a4eaf07b3fa92ae9fe2
Enigmatrix <enigmatrix2000@gmail.com>
2023-12-20 22:54:25 +0800

    feat(query): color tags

---
+++ src/Cobalt/Converters/ColorStringToBrushConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using Avalonia.Media.Immutable;
+
+namespace Cobalt.Converters;
+
+public class ColorStringToBrushConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        var colorStr = (string?)value;
+        if (colorStr == null) return null;
+        return Color.TryParse(colorStr, out var color) ? new ImmutableSolidColorBrush(color) : null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
