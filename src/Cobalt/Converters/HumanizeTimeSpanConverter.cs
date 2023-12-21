3cc711981ac4e471fdf4d656b1f2ba96643c68dc
Enigmatrix <enigmatrix2000@gmail.com>
2023-12-21 20:50:43 +0800

    feat(ui): humanize date time

---
+++ src/Cobalt/Converters/HumanizeTimeSpanConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Humanizer;
+using Humanizer.Localisation;
+
+namespace Cobalt.Converters;
+
+/// <summary>
+///     Converts TimeSpans to human-readable formats
+/// </summary>
+public class HumanizeTimeSpanConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return (value as TimeSpan?)?.Humanize(2, maxUnit: TimeUnit.Day, minUnit: TimeUnit.Second);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
