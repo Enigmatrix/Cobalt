8f43d754233509f29827e6ed2fe660d8cd620f27
Enigmatrix <enigmatrix2000@gmail.com>
2023-12-29 07:18:41 +0800

    feat(vm): add ivalidatorviewmodel validation plugin

---
+++ src/Cobalt/Extensions/NotifyCollectionChangedEx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Reactive.Linq;
+using Avalonia.Collections;
+using DynamicData;
+
+namespace Cobalt.Extensions;
+
+/// <summary>
+///     Extension class for <see cref="INotifyCollectionChanged" />
+/// </summary>
+public static class NotifyCollectionChangedEx
+{
+    /// <summary>
+    ///     Similar to <see cref="NotifyCollectionChangedExtensions.GetWeakCollectionChangedObservable" />
+    ///     combined with
+    ///     <see cref="DynamicData.Binding.ObservableCollectionEx.ToObservableChangeSet{TCollection, T}(TCollection)" />
+    /// </summary>
+    /// <typeparam name="TCollection">The type of collection.</typeparam>
+    /// <typeparam name="T">The type of the object.</typeparam>
+    /// <param name="source">The source.</param>
+    /// <returns>An observable that emits the change set.</returns>
+    public static IObservable<IChangeSet<T>> GetWeakCollectionChangeSet<TCollection, T>(
+        this TCollection source) where TCollection : INotifyCollectionChanged, IEnumerable<T>
+        where T : notnull
+    {
+        var weakCollectionChangedObservable = source.GetWeakCollectionChangedObservable();
+
+        // ref: https://github.com/reactivemarbles/DynamicData/blob/2217b591027424be0c160ca4e5c7f18419f42c63/src/DynamicData/Binding/ObservableCollectionEx.cs#L117
+        return Observable.Create<IChangeSet<T>>(
+            observer =>
+            {
+                var data = new ChangeAwareList<T>(source);
+
+                if (data.Count > 0) observer.OnNext(data.CaptureChanges());
+
+                return weakCollectionChangedObservable.Scan(
+                    data,
+                    (list, args) =>
+                    {
+                        var changes = args;
+
+                        switch (changes.Action)
+                        {
+                            case NotifyCollectionChangedAction.Add when changes.NewItems is not null:
+                            {
+                                if (changes.NewItems.Count == 1 && changes.NewItems[0] is T item)
+                                    list.Insert(changes.NewStartingIndex, item);
+                                else
+                                    list.InsertRange(changes.NewItems.Cast<T>(), changes.NewStartingIndex);
+
+                                break;
+                            }
+
+                            case NotifyCollectionChangedAction.Remove when changes.OldItems is not null:
+                            {
+                                if (changes.OldItems.Count == 1)
+                                    list.RemoveAt(changes.OldStartingIndex);
+                                else
+                                    list.RemoveRange(changes.OldStartingIndex, changes.OldItems.Count);
+
+                                break;
+                            }
+
+                            case NotifyCollectionChangedAction.Replace when changes.NewItems is not null &&
+                                                                            changes.NewItems[0] is T replacedItem:
+                                list[changes.NewStartingIndex] = replacedItem;
+                                break;
+                            case NotifyCollectionChangedAction.Reset:
+                                list.Clear();
+                                // list.AddRange(source);  <------ this is a major difference
+                                break;
+                            case NotifyCollectionChangedAction.Move:
+                                list.Move(changes.OldStartingIndex, changes.NewStartingIndex);
+                                break;
+                        }
+
+                        return list;
+                    }).Select(list => list.CaptureChanges()).SubscribeSafe(observer);
+            });
+    }
+}
---
+++ src/Cobalt/Extensions/ValidatorViewModelValidationPlugin.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reactive.Linq;
+using Avalonia.Data;
+using Avalonia.Data.Core.Plugins;
+using DynamicData;
+using ReactiveUI.Validation.Abstractions;
+using ReactiveUI.Validation.Components.Abstractions;
+using ReactiveUI.Validation.States;
+
+namespace Cobalt.Extensions;
+
+/// <summary>
+///     Validation Plugin for <see cref="IValidatableViewModel" />, based off
+///     <see
+///         href="https://github.com/AvaloniaUI/Avalonia/blob/21ad94b98085767674204e7da71dd2b403759c38/src/Avalonia.Base/Data/Core/Plugins/IndeiValidationPlugin.cs#L13">
+///         the
+///         INDE plugin
+///     </see>
+/// </summary>
+public class ValidatorViewModelValidationPlugin : IDataValidationPlugin
+{
+    [RequiresUnreferencedCode("DataValidationPlugin might require unreferenced code.")]
+    public bool Match(WeakReference<object?> reference, string memberName)
+    {
+        reference.TryGetTarget(out var target);
+
+        return target is IValidatableViewModel;
+    }
+
+    [RequiresUnreferencedCode("DataValidationPlugin might require unreferenced code.")]
+    public IPropertyAccessor Start(WeakReference<object?> reference, string propertyName,
+        IPropertyAccessor inner)
+    {
+        return new Validator(reference, propertyName, inner);
+    }
+
+
+    private class Validator : DataValidationBase
+    {
+        private const bool ExclusivePropertyName = true;
+        private readonly string _name;
+        private readonly WeakReference<object?> _reference;
+        private IDisposable? _validationStatesSub;
+
+        public Validator(WeakReference<object?> reference, string name, IPropertyAccessor inner)
+            : base(inner)
+        {
+            _reference = reference;
+            _name = name;
+        }
+
+        private static IValidationState[] InitialValidationStates { get; } = { ValidationState.Valid };
+
+        protected override void SubscribeCore()
+        {
+            var target = GetReferenceTarget();
+
+            if (target != null)
+            {
+                // ref: https://github.com/reactiveui/ReactiveUI.Validation/blob/540fafebaeebeecd322bd5685caa1f3c70f9ac6d/src/ReactiveUI.Validation/Extensions/ValidationContextExtensions.cs#L51
+                var validationStatesForProperty = target.ValidationContext
+                    .Validations
+                    .GetWeakCollectionChangeSet<ReadOnlyObservableCollection<IValidationComponent>,
+                        IValidationComponent>()
+                    .ToCollection()
+                    .Select(validations => validations
+                        .OfType<IPropertyValidationComponent>()
+                        .Where(validation => validation.ContainsPropertyName(_name, ExclusivePropertyName))
+                        .Select(validation => validation.ValidationStatusChange)
+                        .CombineLatest()
+                        .StartWith(InitialValidationStates))
+                    .Switch();
+
+
+                _validationStatesSub =
+                    validationStatesForProperty.Subscribe(states =>
+                        PublishValue(CreateBindingNotification(Value, states)));
+            }
+
+            base.SubscribeCore();
+        }
+
+        protected override void UnsubscribeCore()
+        {
+            var target = GetReferenceTarget();
+
+            if (target != null) _validationStatesSub?.Dispose();
+
+            base.UnsubscribeCore();
+        }
+
+        protected override void InnerValueChanged(object? value)
+        {
+            PublishValue(CreateBindingNotification(value));
+        }
+
+        private BindingNotification CreateBindingNotification(object? value, IList<IValidationState>? states = null)
+        {
+            List<string>? errors;
+            if (states == null) // exists for InnerValueChanged
+            {
+                var target = GetReferenceTarget();
+                errors = target?.ValidationContext.Validations
+                    .OfType<IPropertyValidationComponent>()
+                    .Where(validation =>
+                        validation.ContainsPropertyName(_name, ExclusivePropertyName) && !validation.IsValid)
+                    .SelectMany(x => x.Text ?? Enumerable.Empty<string>())
+                    .ToList();
+            }
+            else
+            {
+                errors = states.Where(state => !state.IsValid).SelectMany(state => state.Text).ToList();
+            }
+
+            if (errors?.Count > 0)
+                return new BindingNotification(
+                    GenerateException(errors),
+                    BindingErrorType.DataValidationError,
+                    value);
+
+            return new BindingNotification(value);
+        }
+
+        private IValidatableViewModel? GetReferenceTarget()
+        {
+            _reference.TryGetTarget(out var target);
+
+            return target as IValidatableViewModel;
+        }
+
+        private Exception GenerateException(IList<string>? errors)
+        {
+            if (errors!.Count == 1)
+                return new DataValidationException(errors[0]);
+            return new AggregateException(
+                errors.Select(x => new DataValidationException(x)));
+        }
+    }
+}
