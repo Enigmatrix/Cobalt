8be6b32f8a65e5d076eca115b5209b8d15c4dd04
Enigmatrix <enigmatrix2000@gmail.com>
2023-12-29 05:14:32 +0800

    feat(vm): add reactiveobservableobject as a new base

---
+++ src/Cobalt.Common.ViewModels/ReactiveObservableObject.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using ReactiveUI;
+
+namespace Cobalt.Common.ViewModels;
+
+public class ReactiveObservableObject : ObservableObject, IReactiveObject
+{
+    public ReactiveObservableObject()
+    {
+        this.SubscribePropertyChangingEvents();
+        this.SubscribePropertyChangedEvents();
+    }
+
+    public void RaisePropertyChanging(PropertyChangingEventArgs args)
+    {
+        base.OnPropertyChanging(args);
+    }
+
+    public void RaisePropertyChanged(PropertyChangedEventArgs args)
+    {
+        base.OnPropertyChanged(args);
+    }
+
+    protected override void OnPropertyChanging(PropertyChangingEventArgs e)
+    {
+        this.RaisePropertyChanging(e.PropertyName);
+    }
+
+    protected override void OnPropertyChanged(PropertyChangedEventArgs e)
+    {
+        this.RaisePropertyChanged(e.PropertyName);
+    }
+}
