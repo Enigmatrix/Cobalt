8de1eb5b23d315591281267d4fbfaf532f370e86
Enigmatrix <enigmatrix2000@gmail.com>
2024-01-01 15:45:53 +0800

    feat(util): add DU exception

---
+++ src/Cobalt.Common.Util/DiscriminatedUnionException.cs
@@ -0,0 +1,12 @@
+﻿namespace Cobalt.Common.Util;
+
+public class DiscriminatedUnionException<T> : Exception
+{
+    public DiscriminatedUnionException(string name, T? value = default) : base(
+        $"Invalid Discriminated Union state {typeof(T)} for {name}: {value}")
+    {
+        Value = value;
+    }
+
+    public T? Value { get; }
+}
