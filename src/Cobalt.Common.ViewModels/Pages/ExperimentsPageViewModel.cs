30a973dbfd443a0a1cd268290f2c5f66c5cf769e
Enigmatrix <enigmatrix2000@gmail.com>
2024-01-02 21:32:45 +0800

    fix: undo experiment changes

--- src/Cobalt.Common.ViewModels/Pages/ExperimentsPageViewModel.cs
+++ src/Cobalt.Common.ViewModels/Pages/ExperimentsPageViewModel.cs
@@ -1,12 +1,6 @@
-﻿using System.Diagnostics;
-using Cobalt.Common.Data;
-using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Cobalt.Common.Data;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.EntityFrameworkCore;
-using ReactiveUI;
-using ReactiveUI.Validation.Abstractions;
-using ReactiveUI.Validation.Contexts;
-using ReactiveUI.Validation.Extensions;
 
 namespace Cobalt.Common.ViewModels.Pages;
 
@@ -16,14 +10,10 @@ namespace Cobalt.Common.ViewModels.Pages;
 /// <remarks>
 ///     This class does not exist during production.
 /// </remarks>
-public partial class ExperimentsPageViewModel : PageViewModelBase, IValidatableViewModel
+public partial class ExperimentsPageViewModel : PageViewModelBase
 {
-    [ObservableProperty] private TimeSpan? _lmaoDuration = TimeSpan.FromHours(9);
-
     public ExperimentsPageViewModel(IDbContextFactory<QueryContext> contexts) : base(contexts)
     {
-        this.ValidationRule(self => self.LmaoDuration, dur => (dur ?? TimeSpan.Zero) < TimeSpan.FromHours(5), "Too long dick!");
-        this.WhenAnyValue(self => self.LmaoDuration).Subscribe(s => Debug.WriteLine($"dur: {s}"));
     }
 
     public override string Name => "Experiments";
@@ -34,6 +24,4 @@ public partial class ExperimentsPageViewModel : PageViewModelBase, IValidatableV
         await using var context = await Contexts.CreateDbContextAsync();
         await context.UpdateAllUsageEndsAsync(DateTime.Now);
     }
-
-    public ValidationContext ValidationContext { get; } = new();
 }
