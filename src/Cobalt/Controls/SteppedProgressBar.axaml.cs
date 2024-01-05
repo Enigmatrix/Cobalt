839f750c982c44d6200002acbe59876e364ff571
Enigmatrix <enigmatrix2000@gmail.com>
2024-01-05 22:03:46 +0800

    feat(ui): semi-working progressbar with reminders

--- src/Cobalt/Controls/SteppedProgressBar.axaml.cs
+++ src/Cobalt/Controls/SteppedProgressBar.axaml.cs
@@ -1,46 +1,41 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Reactive.Linq;
 using Avalonia.Controls;
 using Avalonia.Data.Converters;
 
-namespace Cobalt.Controls
+namespace Cobalt.Controls;
+
+public partial class SteppedProgressBar : UserControl
 {
-    public partial class SteppedProgressBar : UserControl
+    public SteppedProgressBar()
     {
-        public SteppedProgressBar()
-        {
-            InitializeComponent();
-        }
-
-        public record WithProgressClass(string Text, double Percent) :IWithProgress;
-
-        public interface IWithProgress
-        {
-            double Percent { get; }
-        }
-
-        public IEnumerable<IWithProgress> ItemsSource { get; } = new WithProgressClass[]
-        {
-            new("0", 0),
-            new("A", 0.10),
-            new ("B", 0.5),
-            new("1", 1),
-        };
-
+        InitializeComponent();
     }
 
-        public class PercentToLeftConverter : IMultiValueConverter
-        {
-            public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
-            {
-                if (values.Count != 2) return null;
-                if (values[0] is not double percent || values[1] is not double width) { return null; }
+    public IEnumerable<IWithProgress> ItemsSource { get; } = new WithProgressClass[]
+    {
+        new("0", 0),
+        new("A", 0.10),
+        new("B", 0.5),
+        new("1", 1)
+    };
 
-                return width * percent;
-            }
-        }
+    public record WithProgressClass(string Text, double Percent) : IWithProgress;
+
+    public interface IWithProgress
+    {
+        double Percent { get; }
+    }
+}
+
+public class PercentToLeftConverter : IMultiValueConverter
+{
+    public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (values.Count != 2) return null;
+        if (values[0] is not double percent || values[1] is not double width) return null;
+
+        return width * percent / 100.0;
+    }
 }
