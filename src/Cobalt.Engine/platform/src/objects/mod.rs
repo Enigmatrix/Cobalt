2339146cecbaad00e7ef2ceaf8ad3d3812335ae7
Enigmatrix <enigmatrix2000@gmail.com>
2024-05-12 02:58:25 +0800

    feat(platform): file version info impl

--- src/Cobalt.Engine/platform/src/objects/mod.rs
+++ src/Cobalt.Engine/platform/src/objects/mod.rs
@@ -1,9 +1,11 @@
 mod app_info;
+mod file_version_info;
 mod process;
 mod timestamp;
 mod window;
 
 pub use app_info::*;
+pub use file_version_info::*;
 pub use process::*;
 pub use timestamp::*;
 pub use window::*;
