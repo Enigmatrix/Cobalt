d2eb0dc2463d5646702fbcf33910e54bb5bbdbee
Enigmatrix <enigmatrix2000@gmail.com>
2024-05-12 01:29:29 +0800

    feat(util): use connection string from config

--- src/Cobalt.Engine/util/src/config.rs
+++ src/Cobalt.Engine/util/src/config.rs
@@ -11,6 +11,17 @@ pub struct Config {
     connection_strings: ConnectionStrings,
 }
 
+impl Config {
+    pub fn connection_string(&self) -> &str {
+        &self
+            .connection_strings
+            .query_context
+            .split_once("=")
+            .expect("format of the connection string is Data Source=<path>")
+            .1
+    }
+}
+
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct ConnectionStrings {
