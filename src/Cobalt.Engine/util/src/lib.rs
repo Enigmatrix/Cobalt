1778b9e12d63178db6b5ac3f8fba58fd12ba2a2c
Enigmatrix <enigmatrix2000@gmail.com>
2024-05-12 01:49:22 +0800

    feat(util): use color_eyre instead of anyhow

--- src/Cobalt.Engine/util/src/lib.rs
+++ src/Cobalt.Engine/util/src/lib.rs
@@ -6,6 +6,7 @@ use crate::config::Config;
 use crate::error::Result;
 
 pub fn setup(config: &Config) -> Result<()> {
+    error::setup()?;
     tracing::setup(&config.engine_log_filter())?;
     Ok(())
 }
