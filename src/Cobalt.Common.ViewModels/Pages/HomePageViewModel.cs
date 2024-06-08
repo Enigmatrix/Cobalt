bcbf472600928f7ba3252917494c6040fa96ed3d
Enigmatrix <enigmatrix2000@gmail.com>
2024-06-09 04:11:08 +0800

    feat(ui): add pie charts for day, week and month

--- src/Cobalt.Common.ViewModels/Pages/HomePageViewModel.cs
+++ src/Cobalt.Common.ViewModels/Pages/HomePageViewModel.cs
@@ -1,5 +1,9 @@
-﻿using Cobalt.Common.Data;
+﻿using System.Reactive.Disposables;
+using Cobalt.Common.Data;
+using Cobalt.Common.ViewModels.Analysis;
+using Cobalt.Common.ViewModels.Entities;
 using Microsoft.EntityFrameworkCore;
+using ReactiveUI;
 
 namespace Cobalt.Common.ViewModels.Pages;
 
@@ -8,9 +12,33 @@ namespace Cobalt.Common.ViewModels.Pages;
 /// </summary>
 public class HomePageViewModel : PageViewModelBase
 {
-    public HomePageViewModel(IDbContextFactory<QueryContext> contexts) : base(contexts)
+    public HomePageViewModel(IEntityViewModelCache entityCache, IDbContextFactory<QueryContext> contexts) :
+        base(contexts)
     {
+        AppUsagesPerDay = Query(context => context.AppDurations(context.Apps, DateTime.Today).ToListAsync(),
+            appDur => appDur.Map(entityCache.App));
+
+        AppUsagesPerWeek = Query(
+            context => context.AppDurations(context.Apps, DateTime.Today.AddDays(-(int)DateTime.Today.DayOfWeek))
+                .ToListAsync(),
+            appDur => appDur.Map(entityCache.App));
+
+        AppUsagesPerMonth = Query(
+            context => context.AppDurations(context.Apps, DateTime.Today.AddDays(-DateTime.Today.Day + 1))
+                .ToListAsync(),
+            appDur => appDur.Map(entityCache.App));
+
+        this.WhenActivated((CompositeDisposable dis) =>
+        {
+            AppUsagesPerDay.Refresh();
+            AppUsagesPerWeek.Refresh();
+            AppUsagesPerMonth.Refresh();
+        });
     }
+
+    public Query<List<WithDuration<AppViewModel>>> AppUsagesPerDay { get; }
+    public Query<List<WithDuration<AppViewModel>>> AppUsagesPerWeek { get; }
+    public Query<List<WithDuration<AppViewModel>>> AppUsagesPerMonth { get; }
 
     public override string Name => "Home";
 }
