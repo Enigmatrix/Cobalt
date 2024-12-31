ccde1e2af2de89646d7174a5eb0acc86162c832a
Enigmatrix <enigmatrix2000@gmail.com>
2025-01-01 00:32:42 +0800

    chore: remove c# ui

--- src/Cobalt.Common.Data/IEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Cobalt.Common.Data;
-
-/// <summary>
-///     Trait interface for all Entities
-/// </summary>
-public interface IEntity
-{
-    public long Id { get; }
-}
--- src/Cobalt.Common.Data/IHasDuration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Cobalt.Common.Data;
-
-/// <summary>
-///     Trait interface for classes with a <see cref="Duration" />
-/// </summary>
-public interface IHasDuration
-{
-    public TimeSpan Duration { get; }
-}
