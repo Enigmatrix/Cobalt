82be9122629fecaab7d3e9d7dc8c153088b0d6c1
Enigmatrix <enigmatrix2000@gmail.com>
2025-01-01 19:32:43 +0800

    chore: init shadcn + vite/react-router-v7

---
+++ src/ui/vite.config.ts
@@ -0,0 +1,20 @@
+import { reactRouter } from "@react-router/dev/vite";
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
+
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
+  plugins: [reactRouter(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
