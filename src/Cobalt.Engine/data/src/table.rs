928632b7e85b68a72d413cdd80b81d40b4706834
Enigmatrix <enigmatrix2000@gmail.com>
2025-01-02 07:21:22 +0800

    feat(data): table without columns

--- src/Cobalt.Engine/data/src/table.rs
+++ src/Cobalt.Engine/data/src/table.rs
@@ -2,8 +2,7 @@ use std::fmt::Debug;
 use std::hash::{Hash, Hasher};
 use std::ops::Deref;
 
-use rusqlite::types::{FromSql, FromSqlResult, ToSqlOutput, ValueRef};
-use rusqlite::{Result, ToSql};
+use sqlx::prelude::{FromRow, Type};
 
 /// Trait for mapping an Entity to a [Table] in the database.
 pub trait Table {
@@ -16,19 +15,16 @@ pub trait Table {
 
     /// Name of the [Table]
     fn name() -> &'static str;
-    /// Column names of the [Table]
-    fn columns() -> &'static [&'static str];
 }
 
 /// Reference to a [Table] in the database via its unique identifier.
-#[derive(Default, Debug, Clone)]
-pub struct Ref<T: Table> {
-    inner: T::Id,
-}
+#[derive(Default, Debug, Clone, Type)]
+#[sqlx(transparent)]
+pub struct Ref<T: Table>(T::Id);
 
 impl<T: Table<Id: PartialEq>> PartialEq for Ref<T> {
     fn eq(&self, other: &Self) -> bool {
-        self.inner == other.inner
+        self.0 == other.0
     }
 }
 
@@ -36,14 +32,14 @@ impl<T: Table<Id: Eq>> Eq for Ref<T> {}
 
 impl<T: Table<Id: Hash>> Hash for Ref<T> {
     fn hash<H: Hasher>(&self, state: &mut H) {
-        self.inner.hash(state);
+        self.0.hash(state);
     }
 }
 
 impl<T: Table> Ref<T> {
     /// Create a new [Ref] with the given unique identifier
     pub fn new(inner: T::Id) -> Self {
-        Self { inner }
+        Self(inner)
     }
 }
 
@@ -51,19 +47,7 @@ impl<T: Table> Deref for Ref<T> {
     type Target = T::Id;
 
     fn deref(&self) -> &Self::Target {
-        &self.inner
-    }
-}
-
-impl<Id: ToSql + 'static, T: Table<Id = Id>> ToSql for Ref<T> {
-    fn to_sql(&self) -> Result<ToSqlOutput<'_>> {
-        self.inner.to_sql()
-    }
-}
-
-impl<Id: FromSql + 'static, T: Table<Id = Id>> FromSql for Ref<T> {
-    fn column_result(value: ValueRef<'_>) -> FromSqlResult<Self> {
-        Id::column_result(value).map(|id| Ref::new(id))
+        &self.0
     }
 }
 
@@ -77,7 +61,7 @@ pub type Timestamp = u64;
 pub type Duration = u64;
 
 /// Unique identifier with Version
-#[derive(Default, Debug, Clone, PartialEq, Eq)]
+#[derive(Default, Debug, Clone, PartialEq, Eq, FromRow)]
 pub struct VersionedId {
     pub id: u64,
     pub version: u64,
