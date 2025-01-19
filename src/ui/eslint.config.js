f854ac1118c19c48d2a20d1fbf375c1f198ee10f
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-01-19 15:51:27 +0800

    chore(ui): eslint config

--- src/ui/eslint.config.js
+++ src/ui/eslint.config.js
@@ -9,5 +9,5 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat['jsx-runtime'],
+  pluginReact.configs.flat["jsx-runtime"],
 ];
