095e7252ccf94c4368bdc4964048905d4ff47b2f
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-01-20 05:52:03 +0800

    feat(ui): show basic app list

--- src/ui/app/routes/apps/index.tsx
+++ src/ui/app/routes/apps/index.tsx
@@ -6,8 +6,55 @@ import {
   BreadcrumbPage,
   BreadcrumbList,
 } from "@/components/ui/breadcrumb";
+import { useAppState } from "@/lib/state";
+import type { App } from "@/lib/entities";
+import { useMemo } from "react";
+import { Buffer } from "buffer";
+import { CircleHelp } from "lucide-react";
+
+function AppListItem({ app }: { app: App }) {
+  const icon = useMemo(
+    () =>
+      app.icon
+        ? 'url("data:;base64,' + Buffer.from(app.icon).toString("base64") + '")'
+        : undefined,
+    [app.icon]
+  );
+
+  return (
+    <div className="h-24 shadow-sm rounded-sm bg-muted flex ">
+      <div className="flex-1 flex items-center gap-2 p-4">
+        {icon ? (
+          <div
+            className="mx-2 h-12 w-12 bg-no-repeat bg-center bg-cover"
+            style={{ backgroundImage: icon }}
+          />
+        ) : (
+          <CircleHelp className="mx-2 h-12 w-12 " />
+        )}
+
+        <div className="flex flex-col">
+          <div className="inline-flex place-items-baseline gap-2">
+            <div className="text-lg font-semibold max-w-72 truncate">
+              {app.name}
+            </div>
+            <div>-</div>
+            <div className="text-sm max-w-48 truncate text-white/75">
+              {app.company}
+            </div>
+          </div>
+          <div className="text-sm max-w-[40rem] truncate text-white/50">
+            {app.description}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default function Apps() {
+  const apps = useAppState((state) => state.apps);
+
   return (
     <>
       <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
@@ -21,13 +68,11 @@ export default function Apps() {
           </BreadcrumbList>
         </Breadcrumb>
       </header>
+
       <div className="flex flex-1 flex-col gap-4 p-4">
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-        </div>
-        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+        {Object.values(apps).map((app) => (
+          <AppListItem key={app.id} app={app} />
+        ))}
       </div>
     </>
   );
