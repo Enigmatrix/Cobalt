c317c390ecbf2cc222b568a18b360f80f3e9d151
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-01-23 19:59:17 +0800

    feat(ui): sidebar icons and layout

--- src/ui/app/components/app-sidebar.tsx
+++ src/ui/app/components/app-sidebar.tsx
@@ -6,80 +6,100 @@ import {
   SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
-  SidebarGroupLabel,
+  SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarRail,
 } from "@/components/ui/sidebar";
 import { NavLink, useLocation } from "react-router";
+import { Bell, History, Home, List, Settings, Tag } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 const data = {
   footer: [
     {
       title: "Settings",
       url: "/settings",
+      icon: <Settings />,
     },
   ],
   navMain: [
     {
       title: "Home",
       url: "/",
+      icon: <Home />,
     },
     {
       title: "Apps",
       url: "/apps",
+      icon: <List />,
     },
     {
       title: "Tags",
       url: "/tags",
+      icon: <Tag />,
     },
     {
       title: "Alerts",
       url: "/alerts",
+      icon: <Bell />,
     },
     {
       title: "History",
       url: "/history",
+      icon: <History />,
     },
   ],
 };
+
+function AppSidebarItem({ title, url, icon }: (typeof data.navMain)[0]) {
+  const { pathname } = useLocation();
+  const isActive = url === pathname;
+  return (
+    <SidebarMenuItem key={title}>
+      <SidebarMenuButton asChild isActive={isActive} className="py-2 px-0">
+        <NavLink to={url} className="text-lg inline-flex items-center">
+          <div className="h-4 w-1 ml-1">
+            {isActive ? (
+              <div className="bg-blue-500 h-full rounded-sm w-1"></div>
+            ) : null}
+          </div>
+          {icon}
+          {title}
+        </NavLink>
+      </SidebarMenuButton>
+    </SidebarMenuItem>
+  );
+}
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   const { pathname } = useLocation();
 
   return (
     <Sidebar {...props}>
+      <SidebarHeader></SidebarHeader>
+
       <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {data.navMain.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild isActive={item.url === pathname}>
-                    <NavLink to={item.url}>{item.title}</NavLink>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
+        <SidebarGroupContent>
+          <SidebarMenu>
+            {data.navMain.map((item) => (
+              <AppSidebarItem key={item.title} {...item} />
+            ))}
+          </SidebarMenu>
+        </SidebarGroupContent>
       </SidebarContent>
 
       <SidebarFooter>
-        <SidebarGroup>
+        <SidebarContent>
           <SidebarGroupContent>
             <SidebarMenu>
               {data.footer.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton isActive={item.url === pathname}>
-                    <NavLink to={item.url}>{item.title}</NavLink>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
+                <AppSidebarItem key={item.title} {...item} />
               ))}
             </SidebarMenu>
           </SidebarGroupContent>
-        </SidebarGroup>
+        </SidebarContent>
       </SidebarFooter>
       <SidebarRail />
     </Sidebar>
