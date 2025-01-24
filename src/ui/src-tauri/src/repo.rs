3ccd9c9ada2fa858de361c52cda88a9de95006d5
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-01-24 17:14:15 +0800

    fix(ui): use ts for repo

--- src/ui/src-tauri/src/repo.rs
+++ src/ui/src-tauri/src/repo.rs
@@ -9,14 +9,16 @@ use crate::state::AppState;
 
 #[tauri::command]
 pub async fn get_apps(state: State<'_, AppState>) -> AppResult<HashMap<Ref<App>, infused::App>> {
+    let now = platform::objects::Timestamp::now();
     let mut state = state.lock().await;
-    let res = state.assume_init_mut().repo.get_apps().await?;
+    let res = state.assume_init_mut().repo.get_apps(now).await?;
     Ok(res)
 }
 
 #[tauri::command]
 pub async fn get_tags(state: State<'_, AppState>) -> AppResult<HashMap<Ref<Tag>, infused::Tag>> {
+    let now = platform::objects::Timestamp::now();
     let mut state = state.lock().await;
-    let res = state.assume_init_mut().repo.get_tags().await?;
+    let res = state.assume_init_mut().repo.get_tags(now).await?;
     Ok(res)
 }
