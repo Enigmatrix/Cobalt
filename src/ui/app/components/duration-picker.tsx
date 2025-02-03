3afbae65e89643f6bdc152bd746f1b6340131764
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-02-03 08:22:11 +0800

    feat(ui): indiv textboxes for dur units

--- src/ui/app/components/duration-picker.tsx
+++ src/ui/app/components/duration-picker.tsx
@@ -8,6 +8,10 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { Duration } from "luxon";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import parse from "parse-duration";
+import { Separator } from "./ui/separator";
 
 type DurationPickerProps = {
   className?: string;
@@ -16,8 +20,15 @@ type DurationPickerProps = {
 } & ButtonProps;
 
 const formatHuman = (duration: Duration) => {
-  return "TODO";
+  return duration.rescale().toHuman({
+    unitDisplay: "short",
+  });
 };
+
+function daysStr(duration?: Duration) {
+  const days = duration?.as("days") ?? 0;
+  return Math.floor(days).toString();
+}
 
 export function DurationPicker({
   className,
@@ -25,6 +36,74 @@ export function DurationPicker({
   setDuration,
   ...props
 }: DurationPickerProps) {
+  const [quickInput, setQuickInputInner] = React.useState("");
+  function setQuickInput(value: string) {
+    setQuickInputInner(value);
+    const parsed = parse(value);
+    if (parsed) {
+      const raw = Duration.fromMillis(parsed);
+      const dt = Duration.fromObject({
+        days: Math.floor(raw.as("days")),
+        hours: raw.rescale().hours,
+        minutes: raw.rescale().minutes,
+        seconds: raw.rescale().seconds,
+        milliseconds: raw.rescale().milliseconds,
+      });
+      setDuration(dt);
+      setSecondsInner(dt.seconds.toString());
+      setMinutesInner(dt.minutes.toString());
+      setHoursInner(dt.hours.toString());
+      setDaysInner(daysStr(dt));
+    }
+  }
+
+  const [days, setDaysInner] = React.useState(daysStr(duration));
+  const [hours, setHoursInner] = React.useState(
+    (duration?.hours || 0).toString()
+  );
+  const [minutes, setMinutesInner] = React.useState(
+    (duration?.minutes || 0).toString()
+  );
+  const [seconds, setSecondsInner] = React.useState(
+    (duration?.seconds || 0).toString()
+  );
+
+  const setDays = (value: string) => {
+    setDaysInner(value);
+    handleUnitChange("days", value);
+    setQuickInput("");
+  };
+  const setHours = (value: string) => {
+    setHoursInner(value);
+    handleUnitChange("hours", value);
+    setQuickInput("");
+  };
+  const setMinutes = (value: string) => {
+    setMinutesInner(value);
+    handleUnitChange("minutes", value);
+    setQuickInput("");
+  };
+  const setSeconds = (value: string) => {
+    setSecondsInner(value);
+    handleUnitChange("seconds", value);
+    setQuickInput("");
+  };
+  function resetText(e: React.ChangeEvent<HTMLInputElement>) {
+    if (e.target.value === "") {
+      e.target.value = "0";
+    }
+  }
+
+  function handleUnitChange(unit: keyof Duration, value: string) {
+    setDuration(
+      (duration ?? Duration.fromMillis(0))
+        .set({
+          [unit]: value === "" ? 0 : Number.parseInt(value),
+        })
+        .rescale()
+    );
+  }
+
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -46,8 +125,66 @@ export function DurationPicker({
           )}
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-auto p-0 flex" align="start">
-        TODO
+      <PopoverContent className="w-auto p-4 flex flex-col gap-4" align="start">
+        <div>
+          <Label className="font-semibold">Quick input</Label>
+          <Input
+            value={quickInput}
+            onChange={(e) => setQuickInput(e.target.value)}
+            placeholder="e.g. 2d 4h 30m"
+            className="mt-2"
+          />
+        </div>
+        <Separator orientation="horizontal" />
+        <div className="grid gap-2">
+          <div className="grid grid-cols-3 items-center gap-4">
+            <Label>Days</Label>
+            <Input
+              type="number"
+              value={days}
+              onChange={(e) => setDays(e.target.value)}
+              min={0}
+              className="col-span-2"
+              onBlur={resetText}
+            />
+          </div>
+          <div className="grid grid-cols-3 items-center gap-4">
+            <Label>Hours</Label>
+            <Input
+              type="number"
+              value={hours}
+              onChange={(e) => setHours(e.target.value)}
+              min={0}
+              max={23}
+              className="col-span-2"
+              onBlur={resetText}
+            />
+          </div>
+          <div className="grid grid-cols-3 items-center gap-4">
+            <Label>Minutes</Label>
+            <Input
+              type="number"
+              value={minutes}
+              onChange={(e) => setMinutes(e.target.value)}
+              min={0}
+              max={59}
+              className="col-span-2"
+              onBlur={resetText}
+            />
+          </div>
+          <div className="grid grid-cols-3 items-center gap-4">
+            <Label>Seconds</Label>
+            <Input
+              type="number"
+              value={seconds}
+              onChange={(e) => setSeconds(e.target.value)}
+              min={0}
+              max={59}
+              className="col-span-2"
+              onBlur={resetText}
+            />
+          </div>
+        </div>
       </PopoverContent>
     </Popover>
   );
