f6d31e4643dd00d4e1ba6bad75f7041fa84a4157
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-02-04 00:54:20 +0800

    feat(ui): format human dt

--- src/ui/app/lib/time.ts
+++ src/ui/app/lib/time.ts
@@ -26,3 +26,14 @@ export function ticksToDuration(ts: number): Duration {
 export function ticksToDateTime(ticks: number): DateTime {
   return DateTime.fromMillis(ticks / 10_000 - 62_135_596_800_000);
 }
+
+export function toHumanDateTime(dt: DateTime): string {
+  // if time part is 00:00:00, then return date part only
+  if (dt.toFormat("HH:mm:ss") === "00:00:00") {
+    return dt.toFormat("LLL dd, y");
+  }
+  if (dt.toFormat("ss") === "00") {
+    return dt.toFormat("LLL dd, y hh:mm a");
+  }
+  return dt.toFormat("LLL dd, y hh:mm:ss a");
+}
