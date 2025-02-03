d03ca9079c218dc311b6a5454f94739b1227caa4
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-02-03 09:05:06 +0800

    feat(ui): date time range picker, remove outer div

--- src/ui/app/components/date-time-range-picker.tsx
+++ src/ui/app/components/date-time-range-picker.tsx
@@ -3,7 +3,7 @@ import { Calendar as CalendarIcon } from "lucide-react";
 import type { DateRange as RDateRange } from "react-day-picker";
 
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
+import { Button, type ButtonProps } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import {
   Popover,
@@ -21,7 +21,7 @@ type DateTimeRangePickerProps = {
   date?: DateRange;
   setDate: React.Dispatch<React.SetStateAction<DateRange | undefined>>;
   disabled?: boolean;
-} & React.HTMLAttributes<HTMLDivElement>;
+} & ButtonProps;
 
 export type DateRange = RDateRange;
 
@@ -99,178 +99,178 @@ export function DateTimeRangePicker({
     [setDateInner]
   );
   return (
-    <div className={cn("grid gap-2", className)} {...props}>
-      <Popover>
-        <PopoverTrigger asChild disabled={disabled}>
-          <Button
-            id="date"
-            variant={"outline"}
-            className={cn(
-              "min-w-[300px] justify-start text-left font-normal",
-              !date && "text-muted-foreground"
-            )}
-          >
-            <CalendarIcon />
-            {date?.from ? (
-              date.to ? (
-                <>
-                  {formatHuman(date.from)} - {formatHuman(date.to)}
-                </>
-              ) : (
-                <>
-                  {formatHuman(date.from)} -{" "}
-                  <div className="text-muted-foreground">Pick end time</div>
-                </>
-              )
+    <Popover>
+      <PopoverTrigger asChild disabled={disabled}>
+        <Button
+          id="date"
+          variant={"outline"}
+          className={cn(
+            "min-w-[300px] justify-start text-left font-normal",
+            !date && "text-muted-foreground",
+            className
+          )}
+          {...props}
+        >
+          <CalendarIcon />
+          {date?.from ? (
+            date.to ? (
+              <>
+                {formatHuman(date.from)} - {formatHuman(date.to)}
+              </>
             ) : (
-              <span>Pick a time range</span>
-            )}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-auto p-0 flex" align="start">
-          <div className="flex flex-col">
-            <div className="flex">
-              <div className="flex-1 p-4">
-                <Label>From</Label>
-                <Input
-                  type="datetime-local"
-                  step="1"
-                  className="mt-2"
-                  value={fromStr}
-                  onChange={(e) => setFromStr(e.target.value)}
-                />
-              </div>
-              <div className="flex-1 p-4">
-                <Label>To</Label>
-                <Input
-                  type="datetime-local"
-                  step="1"
-                  className="mt-2"
-                  value={toStr}
-                  onChange={(e) => setToStr(e.target.value)}
-                />
-              </div>
+              <>
+                {formatHuman(date.from)} -{" "}
+                <div className="text-muted-foreground">Pick end time</div>
+              </>
+            )
+          ) : (
+            <span>Pick a time range</span>
+          )}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0 flex" align="start">
+        <div className="flex flex-col">
+          <div className="flex">
+            <div className="flex-1 p-4">
+              <Label>From</Label>
+              <Input
+                type="datetime-local"
+                step="1"
+                className="mt-2"
+                value={fromStr}
+                onChange={(e) => setFromStr(e.target.value)}
+              />
             </div>
-            <Calendar
-              initialFocus
-              mode="range"
-              defaultMonth={date?.from}
-              selected={date}
-              onSelect={setDate}
-              numberOfMonths={2}
-            />
+            <div className="flex-1 p-4">
+              <Label>To</Label>
+              <Input
+                type="datetime-local"
+                step="1"
+                className="mt-2"
+                value={toStr}
+                onChange={(e) => setToStr(e.target.value)}
+              />
+            </div>
           </div>
-          <div className="flex flex-col p-2 gap-2">
-            <Button
-              variant="outline"
-              size="sm"
-              className="w-full"
-              onClick={() =>
-                setDate({
-                  from: now.startOf("day").toJSDate(),
-                  to: now.plus({ day: 1 }).startOf("day").toJSDate(),
-                })
-              }
-            >
-              Today
-            </Button>
-            <Button
-              variant="outline"
-              size="sm"
-              className="w-full"
-              onClick={() =>
-                setDate({
-                  from: now.minus({ day: 1 }).startOf("day").toJSDate(),
-                  to: now.startOf("day").toJSDate(),
-                })
-              }
-            >
-              Yesterday
-            </Button>
-            <Separator />
-            <Button
-              variant="outline"
-              size="sm"
-              className="w-full"
-              onClick={() =>
-                setDate({
-                  from: now.startOf("week").toJSDate(),
-                  to: now.plus({ week: 1 }).startOf("week").toJSDate(),
-                })
-              }
-            >
-              This Week
-            </Button>
-            <Button
-              variant="outline"
-              size="sm"
-              className="w-full"
-              onClick={() =>
-                setDate({
-                  from: now.minus({ week: 1 }).startOf("week").toJSDate(),
-                  to: now.startOf("week").toJSDate(),
-                })
-              }
-            >
-              Last Week
-            </Button>
-            <Separator />
-            <Button
-              variant="outline"
-              size="sm"
-              className="w-full"
-              onClick={() =>
-                setDate({
-                  from: now.startOf("month").toJSDate(),
-                  to: now.plus({ month: 1 }).startOf("month").toJSDate(),
-                })
-              }
-            >
-              This Month
-            </Button>
-            <Button
-              variant="outline"
-              size="sm"
-              className="w-full"
-              onClick={() =>
-                setDate({
-                  from: now.minus({ month: 1 }).startOf("month").toJSDate(),
-                  to: now.startOf("month").toJSDate(),
-                })
-              }
-            >
-              Last Month
-            </Button>
-            <Separator />
-            <Button
-              variant="outline"
-              size="sm"
-              className="w-full"
-              onClick={() =>
-                setDate({
-                  from: now.startOf("year").toJSDate(),
-                  to: now.plus({ year: 1 }).startOf("year").toJSDate(),
-                })
-              }
-            >
-              This Year
-            </Button>
-            <Button
-              variant="outline"
-              size="sm"
-              className="w-full"
-              onClick={() =>
-                setDate({
-                  from: now.minus({ year: 1 }).startOf("year").toJSDate(),
-                  to: now.startOf("year").toJSDate(),
-                })
-              }
-            >
-              Last Year
-            </Button>
-          </div>
-        </PopoverContent>
-      </Popover>
-    </div>
+          <Calendar
+            initialFocus
+            mode="range"
+            defaultMonth={date?.from}
+            selected={date}
+            onSelect={setDate}
+            numberOfMonths={2}
+          />
+        </div>
+        <div className="flex flex-col p-2 gap-2">
+          <Button
+            variant="outline"
+            size="sm"
+            className="w-full"
+            onClick={() =>
+              setDate({
+                from: now.startOf("day").toJSDate(),
+                to: now.plus({ day: 1 }).startOf("day").toJSDate(),
+              })
+            }
+          >
+            Today
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            className="w-full"
+            onClick={() =>
+              setDate({
+                from: now.minus({ day: 1 }).startOf("day").toJSDate(),
+                to: now.startOf("day").toJSDate(),
+              })
+            }
+          >
+            Yesterday
+          </Button>
+          <Separator />
+          <Button
+            variant="outline"
+            size="sm"
+            className="w-full"
+            onClick={() =>
+              setDate({
+                from: now.startOf("week").toJSDate(),
+                to: now.plus({ week: 1 }).startOf("week").toJSDate(),
+              })
+            }
+          >
+            This Week
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            className="w-full"
+            onClick={() =>
+              setDate({
+                from: now.minus({ week: 1 }).startOf("week").toJSDate(),
+                to: now.startOf("week").toJSDate(),
+              })
+            }
+          >
+            Last Week
+          </Button>
+          <Separator />
+          <Button
+            variant="outline"
+            size="sm"
+            className="w-full"
+            onClick={() =>
+              setDate({
+                from: now.startOf("month").toJSDate(),
+                to: now.plus({ month: 1 }).startOf("month").toJSDate(),
+              })
+            }
+          >
+            This Month
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            className="w-full"
+            onClick={() =>
+              setDate({
+                from: now.minus({ month: 1 }).startOf("month").toJSDate(),
+                to: now.startOf("month").toJSDate(),
+              })
+            }
+          >
+            Last Month
+          </Button>
+          <Separator />
+          <Button
+            variant="outline"
+            size="sm"
+            className="w-full"
+            onClick={() =>
+              setDate({
+                from: now.startOf("year").toJSDate(),
+                to: now.plus({ year: 1 }).startOf("year").toJSDate(),
+              })
+            }
+          >
+            This Year
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            className="w-full"
+            onClick={() =>
+              setDate({
+                from: now.minus({ year: 1 }).startOf("year").toJSDate(),
+                to: now.startOf("year").toJSDate(),
+              })
+            }
+          >
+            Last Year
+          </Button>
+        </div>
+      </PopoverContent>
+    </Popover>
   );
 }
