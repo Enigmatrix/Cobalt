cc2fdd1f2e7d20ac7eb32a70ce55ae78b00a2539
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-02-06 15:55:15 +0800

    feat(ui): zod form hook

---
+++ src/ui/app/hooks/use-fom.tsx
@@ -0,0 +1,17 @@
+import { type UseFormProps, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { type z } from "zod";
+
+export const useZodForm = <TSchema extends z.ZodType>({
+  schema,
+  ...props
+}: Omit<UseFormProps<TSchema["_input"]>, "resolver"> & {
+  schema: TSchema;
+}) => {
+  const form = useForm<TSchema["_input"]>({
+    ...props,
+    resolver: zodResolver(schema, undefined),
+  });
+
+  return form;
+};
