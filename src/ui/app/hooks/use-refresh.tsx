d8f3c877d0c66a6f7be0bfa167162e5db336a81c
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-02-06 16:13:24 +0800

    feat(ui): handle stale

--- src/ui/app/hooks/use-refresh.tsx
+++ src/ui/app/hooks/use-refresh.tsx
@@ -1,7 +1,21 @@
 import { useAppState, refresh } from "@/lib/state";
+import { useCallback } from "react";
 
 export function useRefresh() {
   const lastRefresh = useAppState((state) => state.lastRefresh);
+  // TODO: queue a debounced refresh?
+  const handleStale = useCallback(
+    function <T>(data: (T | undefined)[]) {
+      return data.filter((x) => x !== undefined);
+    },
+    [lastRefresh]
+  );
 
-  return { refreshToken: lastRefresh, refresh };
+  return {
+    refreshToken: lastRefresh,
+    refresh,
+    handleStaleApps: handleStale,
+    handleStaleTags: handleStale,
+    handleStaleAlerts: handleStale,
+  };
 }
