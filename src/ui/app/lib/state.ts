5d242f12585d86a43e311701547fc5138e90a4bf
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-02-09 03:53:02 +0800

    feat(ui): state methods to maintain app_tags and update

--- src/ui/app/lib/state.ts
+++ src/ui/app/lib/state.ts
@@ -3,9 +3,16 @@ import { create } from "zustand";
 import type { App, Ref, Tag } from "@/lib/entities";
 import { DateTime } from "luxon";
 import { dateTimeToTicks } from "@/lib/time";
-import { getApps, getTags } from "@/lib/repo";
+import {
+  addAppTag,
+  getApps,
+  getTags,
+  removeAppTag,
+  updateApp,
+} from "@/lib/repo";
 import { checkForUpdatesBackground } from "@/lib/updater";
 import { info } from "@/lib/log";
+import { produce } from "immer";
 
 export async function initState() {
   // init rust-side state
@@ -43,6 +50,9 @@ type AppState = {
   setApps: (apps: EntityStore<App>) => void;
   setTags: (tags: EntityStore<Tag>) => void;
   setLastRefresh: (lastRefresh: DateTime) => void;
+  updateApp: (app: App) => Promise<void>;
+  addAppTag: (appId: Ref<App>, tagId: Ref<Tag>) => Promise<void>;
+  removeAppTag: (appId: Ref<App>, tagId: Ref<Tag>) => Promise<void>;
 };
 
 export const useAppState = create<AppState>((set) => {
@@ -53,5 +63,40 @@ export const useAppState = create<AppState>((set) => {
     setApps: (apps) => set({ apps }),
     setTags: (tags) => set({ tags }),
     setLastRefresh: (lastRefresh) => set({ lastRefresh }),
+
+    updateApp: async (app) => {
+      await updateApp(app);
+
+      set((state) =>
+        produce(
+          (draft) => (draft.apps[app.id] = { ...draft.apps[app.id], ...app }),
+        )(state),
+      );
+    },
+
+    addAppTag: async (appId, tagId) => {
+      await addAppTag(appId, tagId);
+
+      set((state) =>
+        produce((draft: AppState) => {
+          if (draft.apps[appId]!.tags.includes(tagId)) {
+            return;
+          }
+          draft.apps[appId]!.tags.push(tagId);
+        })(state),
+      );
+    },
+
+    removeAppTag: async (appId, tagId) => {
+      await removeAppTag(appId, tagId);
+
+      set((state) =>
+        produce((draft: AppState) => {
+          draft.apps[appId]!.tags = draft.apps[appId]!.tags.filter(
+            (id: Ref<Tag>) => id !== tagId,
+          );
+        })(state),
+      );
+    },
   };
 });
