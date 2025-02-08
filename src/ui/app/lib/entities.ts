f0658f550b489104980c0f28cb912e933a3de620
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-02-09 01:33:50 +0800

    feat(ui): add type guard functions for AppIdentity

--- src/ui/app/lib/entities.ts
+++ src/ui/app/lib/entities.ts
@@ -19,6 +19,18 @@ export type AppIdentity =
   | { Uwp: { aumid: string } }
   | { Win32: { path: string } };
 
+export function isUwp(
+  identity: AppIdentity,
+): identity is { Uwp: { aumid: string } } {
+  return "Uwp" in identity;
+}
+
+export function isWin32(
+  identity: AppIdentity,
+): identity is { Win32: { path: string } } {
+  return "Win32" in identity;
+}
+
 export interface UsageInfo {
   usage_today: number;
   usage_week: number;
