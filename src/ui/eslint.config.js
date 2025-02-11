e487f6dcce9b7dba47f1fd961a137c50fdef13ca
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-02-12 07:41:42 +0800

    chore(ui): configure hooks eslint

--- src/ui/eslint.config.js
+++ src/ui/eslint.config.js
@@ -2,6 +2,9 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat();
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -10,4 +13,5 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat["jsx-runtime"],
+  ...compat.extends("plugin:react-hooks/recommended"),
 ];
