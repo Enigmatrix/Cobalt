f279ce49b194875e11141507aab4930f55d8c206
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-02-21 09:03:58 +0800

    feat(hooks): add useRepo general helper

--- src/ui/app/hooks/use-repo.tsx
+++ src/ui/app/hooks/use-repo.tsx
@@ -1,10 +1,51 @@
 import type { App, Ref, Tag, WithGroupedDuration } from "@/lib/entities";
-import { getAppDurationsPerPeriod, getTagDurationsPerPeriod } from "@/lib/repo";
+import {
+  getAppDurationsPerPeriod,
+  getAppSessionUsages,
+  getInteractionPeriods,
+  getTagDurationsPerPeriod,
+} from "@/lib/repo";
 import type { EntityMap } from "@/lib/state";
 import _ from "lodash";
 import type { DateTime, Duration } from "luxon";
 import { useEffect, useState, useTransition } from "react";
 import { useRefresh } from "@/hooks/use-refresh";
+
+export function useRepo<T, Arg extends object>(
+  fn: (args: Arg) => Promise<T>,
+  def: T,
+  arg: Arg,
+) {
+  const { refreshToken } = useRefresh();
+  const [ret, setRet] = useState<T>(def);
+  const [isLoading, startTransition] = useTransition();
+  useEffect(() => {
+    startTransition(async () => {
+      const result = await fn(arg);
+      setRet(result);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    refreshToken,
+    startTransition,
+    setRet,
+    fn,
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    ...Object.entries(arg).flat(),
+  ]);
+  return { ret, isLoading };
+}
+
+// fn is a function that takes one argument and returns a promise
+export function makeUseRepo<T, Arg extends object>(
+  fn: (args: Arg) => Promise<T>,
+  def: T,
+) {
+  return (arg: Arg) => useRepo<T, Arg>(fn, def, arg);
+}
+
+export const useAppSessionUsages = makeUseRepo(getAppSessionUsages, {});
+export const useInteractionPeriods = makeUseRepo(getInteractionPeriods, []);
 
 export function useAppDurationsPerPeriod({
   start,
