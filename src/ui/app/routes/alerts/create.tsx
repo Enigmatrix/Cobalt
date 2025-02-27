2001f4d807958c7d750f6e0583bb60b580ca869e
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-02-27 09:14:04 +0800

    feat(ui): add skeletal create alert form and routing

---
+++ src/ui/app/routes/alerts/create.tsx
@@ -0,0 +1,11 @@
+import { CreateAlertForm } from "@/components/alert/create-alert-form";
+
+export default function CreateAlerts() {
+  return (
+    <>
+      <main>
+        <CreateAlertForm />
+      </main>
+    </>
+  );
+}
