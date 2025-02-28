a6d8e49613dbf7e829b5aeab2c8b8cef68504e6a
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-02-28 10:45:40 +0800

    feat(tag): add apps list to CreateTag and update tag associations

--- src/ui/app/lib/repo.ts
+++ src/ui/app/lib/repo.ts
@@ -22,6 +22,7 @@ import { dateTimeToTicks, durationToTicks } from "@/lib/time";
 export interface CreateTag {
   name: string;
   color: string;
+  apps: Ref<App>[];
 }
 
 export type AppSessionUsages = {
