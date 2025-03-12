a59b9ddeb87e55bc2cb78ad3553c68cd93adefea
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-03-12 22:24:07 +0800

    chore(ui): add tauri single-instance

--- src/ui/src-tauri/src/lib.rs
+++ src/ui/src-tauri/src/lib.rs
@@ -9,6 +9,7 @@ mod tracing;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init())
         .plugin(tauri_plugin_opener::init())
         .setup(|app| {
             #[cfg(desktop)]
