deb8b02560669acdc48f4cd421a7ebe5abd039b3
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-03-13 22:16:05 +0800

    feat(ui) better sidebar navigation

--- src/ui/app/components/app-sidebar.tsx
+++ src/ui/app/components/app-sidebar.tsx
@@ -21,9 +21,7 @@ import {
   List,
   Settings,
   Tag,
-  ActivityIcon,
   ArrowLeftIcon,
-  ArrowRightIcon,
 } from "lucide-react";
 
 const data = {
@@ -78,7 +76,11 @@ function AppSidebarItem({ title, url, icon }: (typeof data.navMain)[0]) {
   return (
     <SidebarMenuItem key={title}>
       <SidebarMenuButton asChild isActive={isActive} className="py-2 px-0">
-        <NavLink to={url} className="text-lg inline-flex items-center">
+        <NavLink
+          to={url}
+          className="text-lg inline-flex items-center rounded-none"
+        >
+          {/* Active indicator */}
           <div className="h-4 w-1 ml-1">
             {isActive ? (
               <div className="bg-primary h-full rounded-sm w-1"></div>
@@ -95,31 +97,18 @@ function AppSidebarItem({ title, url, icon }: (typeof data.navMain)[0]) {
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   const navigate = useNavigate();
   const canGoBack = window.history.state && window.history.state.idx > 0;
-  const canGoForward =
-    window.history.state &&
-    window.history.state.idx < window.history.length - 1;
 
   return (
     <Sidebar {...props}>
       <SidebarHeader>
         <div className="flex items-center">
-          <ActivityIcon className="mx-2" size={18} />
-          <div className="text-base mr-auto ml-2">Cobalt</div>
           <Button
             variant="ghost"
             size="icon"
             disabled={!canGoBack}
             onClick={() => navigate(-1)}
           >
-            <ArrowLeftIcon className="h-4 w-4" />
-          </Button>
-          <Button
-            variant="ghost"
-            size="icon"
-            disabled={!canGoForward}
-            onClick={() => navigate(1)}
-          >
-            <ArrowRightIcon className="h-4 w-4" />
+            <ArrowLeftIcon />
           </Button>
         </div>
       </SidebarHeader>
