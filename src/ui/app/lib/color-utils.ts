371c97f0856997bdee25a155deb044f949798abc
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-03-25 07:44:09 +0800

    feat(ui): better color var extraction

--- src/ui/app/lib/color-utils.ts
+++ src/ui/app/lib/color-utils.ts
@@ -1,5 +1,5 @@
 type RGB = { r: number; g: number; b: number };
-type HSL = { h: number; s: number; l: number };
+import α from "color-alpha";
 
 // ref: https://github.com/catppuccin/catppuccin
 // Mocha colors
@@ -36,52 +36,10 @@ export const hexToRgb = (hex: string): RGB | null => {
     : null;
 };
 
-export const hslToRgb = ({ h, s, l }: HSL): RGB => {
-  h /= 360;
-  s /= 100;
-  l /= 100;
-
-  let r: number, g: number, b: number;
-
-  if (s === 0) {
-    r = g = b = l;
-  } else {
-    const hue2rgb = (p: number, q: number, t: number) => {
-      if (t < 0) t += 1;
-      if (t > 1) t -= 1;
-      if (t < 1 / 6) return p + (q - p) * 6 * t;
-      if (t < 1 / 2) return q;
-      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-      return p;
-    };
-
-    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-    const p = 2 * l - q;
-
-    r = hue2rgb(p, q, h + 1 / 3);
-    g = hue2rgb(p, q, h);
-    b = hue2rgb(p, q, h - 1 / 3);
-  }
-
-  return {
-    r: Math.round(r * 255),
-    g: Math.round(g * 255),
-    b: Math.round(b * 255),
-  };
-};
-
-export const formatRgba = (rgb: RGB, a = 1): string => {
-  return `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, ${a})`;
-};
-
 export const getVarColorAsHex = (varName: string, a = 1): string => {
   const varVal = getComputedStyle(document.documentElement).getPropertyValue(
     "--" + varName,
   );
-  const [h, s, l] = varVal
-    .split("(")[1]
-    .split(")")[0]
-    .split(" ")
-    .map((v) => parseFloat(v));
-  return formatRgba(hslToRgb({ h, s, l }), a);
+
+  return α(varVal, a);
 };
