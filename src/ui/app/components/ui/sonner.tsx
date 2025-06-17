c61559a574b65cc6cf19516ef8865bb6bd9fbc90
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-06-17 19:28:58 +0800

    fix(ui): remove next-themes

--- src/ui/app/components/ui/sonner.tsx
+++ src/ui/app/components/ui/sonner.tsx
@@ -1,4 +1,4 @@
-import { useTheme } from "next-themes"
+import { useTheme } from "@/components/theme-provider"
 import { Toaster as Sonner, type ToasterProps } from "sonner"
 
 const Toaster = ({ ...props }: ToasterProps) => {
