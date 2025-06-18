bed58ebaa9d6372cb0674c2d92741b08afa66496
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-06-18 12:39:57 +0800

    fix(ui): indeterminate checkbox

--- src/ui/app/components/ui/checkbox.tsx
+++ src/ui/app/components/ui/checkbox.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
-import { CheckIcon } from "lucide-react"
+import { CheckIcon, MinusIcon } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
@@ -23,7 +23,11 @@ function Checkbox({
         data-slot="checkbox-indicator"
         className="flex items-center justify-center text-current transition-none"
       >
-        <CheckIcon className="size-3.5" />
+        {props.checked === "indeterminate" ? (
+          <MinusIcon className="size-3.5" />
+        ) : (
+          <CheckIcon className="size-3.5" />
+        )}
       </CheckboxPrimitive.Indicator>
     </CheckboxPrimitive.Root>
   )
