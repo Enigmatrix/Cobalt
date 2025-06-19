1ad61738deb89177481278809cc38df19aac059e
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-06-19 22:04:55 +0800

    feat(util): adjust error handling hooks for debug and release modes

--- src/util/src/error.rs
+++ src/util/src/error.rs
@@ -9,7 +9,12 @@ use crate::tracing::error;
 pub fn setup() -> color_eyre::Result<()> {
     // the equivalent of color_eyre::install()
 
+    #[cfg(debug_assertions)]
     let (panic_hook, eyre_hook) = config::HookBuilder::default().into_hooks();
+    #[cfg(not(debug_assertions))]
+    let (panic_hook, eyre_hook) = config::HookBuilder::default()
+        .theme(config::Theme::new()) // blank theme to disable color
+        .into_hooks();
     eyre_hook.install()?;
     // panic_hook.install();
 
@@ -17,8 +22,8 @@ pub fn setup() -> color_eyre::Result<()> {
     // add a custom panic hook that logs the panic and exits
     panic::set_hook(Box::new(move |panic_info| {
         let report = panic_hook.panic_report(panic_info).to_string();
-        error!("unhandled panic: {}", report);
-        eprintln!("{}", report);
+        error!("unhandled panic: {report}");
+        eprintln!("{report}");
         std::process::exit(1);
     }));
     Ok(())
