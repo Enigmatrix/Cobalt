48352ef34c1f74d85bb397dbfa730f5f2d719f8f
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-07-04 06:35:53 +0800

    feat(platform): shift browser window state fetch to its own fn

--- src/platform/src/events/foreground.rs
+++ src/platform/src/events/foreground.rs
@@ -2,7 +2,7 @@ use std::time::Duration;
 
 use util::config::Config;
 use util::error::{Context, Result, eyre};
-use util::tracing::ResultTraceExt;
+use util::tracing::{ResultTraceExt, debug, info};
 
 use crate::objects::{Process, Timestamp, Window};
 use crate::web::{BrowserDetector, BrowserWindowInfo, State, WriteLockedState};
@@ -48,79 +48,97 @@ impl ForegroundEventWatcher {
         })
     }
 
-    /// Get the foreground window session.
-    pub fn foreground_window_session(
+    fn browser_window_info(
+        fg: &Window,
         browser: &BrowserDetector,
         mut browser_state: WriteLockedState<'_>,
         track_incognito: bool,
+    ) -> Result<(Option<BrowserWindowInfo>, Option<String>)> {
+        let browser_window_info = browser_state.browser_windows.get(fg).cloned();
+        if let Some(browser_window_info) = browser_window_info {
+            debug!("window {fg:?} has entry in window states");
+            // We know already if it's a browser or not
+            return Ok((browser_window_info, None));
+        }
+
+        // Educated guess. Note that VSCode will pass this check since it's a chromium app.
+        let maybe_browser = browser.is_maybe_chromium_window(fg).warn();
+        // So we further check if it's a chromium process
+        let (is_browser, fetched_path) = if maybe_browser {
+            let ptid = fg.ptid().context("get ptid")?;
+            if browser_state.browser_processes.contains(&ptid.pid) {
+                info!("process {:?} is in browser_processes", ptid.pid);
+                (true, None)
+            } else {
+                info!(
+                    "process {:?} is not in browser_processes, fetching path",
+                    ptid.pid
+                );
+                let process = Process::new(ptid.pid).context("get process")?;
+                let path = process.path().context("get process path")?;
+                let is_browser = BrowserDetector::is_maybe_chromium_exe(&path);
+                if is_browser {
+                    browser_state.browser_processes.insert(ptid.pid);
+                }
+                (is_browser, Some(path))
+            }
+        } else {
+            info!("window {fg:?} is not a chromium window");
+            (false, None)
+        };
+
+        let browser_window_info = if is_browser {
+            info!("getting new chromium window info for {fg:?}");
+            let element = browser.get_chromium_element(fg)?;
+
+            // Loop until we get a valid incognito state. Invalid states are when the broser is loading
+            // from memory save, for example - the browser is not fully loaded at that time. Max timeout is 3 seconds.
+            let is_incognito = loop_until_valid(
+                || {
+                    let incognito = browser
+                        .chromium_incognito(&element)
+                        .context("get chromium incognito")?;
+
+                    Ok(incognito)
+                },
+                Duration::from_millis(100),
+                Duration::from_secs(3),
+            )?;
+
+            // TODO: put this in loop_until_valid
+            let url = if is_incognito && !track_incognito {
+                None
+            } else {
+                let element = browser.get_chromium_element(fg)?;
+
+                browser
+                    .chromium_url(&element, None)
+                    .context("get chromium url")?
+            };
+
+            Some(BrowserWindowInfo { is_incognito, url })
+        } else {
+            None
+        };
+
+        info!("inserting new browser window info for {fg:?}: {browser_window_info:?}");
+
+        browser_state
+            .browser_windows
+            .insert(fg.clone(), browser_window_info.clone());
+
+        Ok((browser_window_info, fetched_path))
+    }
+
+    /// Get the foreground window session.
+    pub fn foreground_window_session(
+        browser: &BrowserDetector,
+        browser_state: WriteLockedState<'_>,
+        track_incognito: bool,
     ) -> Result<Option<WindowSession>> {
         if let Some(fg) = Window::foreground() {
-            let (browser_window_info, fetched_path) = {
-                let browser_window_info = browser_state.browser_windows.get(&fg).cloned();
-                if let Some(browser_window_info) = browser_window_info {
-                    // We know already if it's a browser or not
-                    (browser_window_info, None)
-                } else {
-                    // Educated guess. Note that VSCode will pass this check since it's a chromium app.
-                    let maybe_browser = browser.is_maybe_chromium_window(&fg).warn();
-                    // So we further check if it's a chromium process
-                    let (is_browser, fetched_path) = if maybe_browser {
-                        let ptid = fg.ptid().context("get ptid")?;
-                        if browser_state.browser_processes.contains(&ptid.pid) {
-                            (true, None)
-                        } else {
-                            let process = Process::new(ptid.pid).context("get process")?;
-                            let path = process.path().context("get process path")?;
-                            let is_browser = BrowserDetector::is_maybe_chromium_exe(&path);
-                            if is_browser {
-                                browser_state.browser_processes.insert(ptid.pid);
-                            }
-                            (is_browser, Some(path))
-                        }
-                    } else {
-                        (false, None)
-                    };
-
-                    let browser_window_info = if is_browser {
-                        let element = browser.get_chromium_element(&fg)?;
-
-                        // Loop until we get a valid incognito state. Invalid states are when the broser is loading
-                        // from memory save, for example - the browser is not fully loaded at that time. Max timeout is 3 seconds.
-                        let is_incognito = loop_until_valid(
-                            || {
-                                let incognito = browser
-                                    .chromium_incognito(&element)
-                                    .context("get chromium incognito")?;
-
-                                Ok(incognito)
-                            },
-                            Duration::from_millis(100),
-                            Duration::from_secs(3),
-                        )?;
-
-                        let url = if is_incognito && !track_incognito {
-                            None
-                        } else {
-                            let element = browser.get_chromium_element(&fg)?;
-                            let url = browser
-                                .chromium_url(&element, None)
-                                .context("get chromium url")?;
-                            url
-                        };
-
-                        Some(BrowserWindowInfo { is_incognito, url })
-                    } else {
-                        None
-                    };
-
-                    browser_state
-                        .browser_windows
-                        .insert(fg.clone(), browser_window_info.clone());
-
-                    (browser_window_info, fetched_path)
-                }
-            };
-            drop(browser_state);
+            let (browser_window_info, fetched_path) =
+                Self::browser_window_info(&fg, browser, browser_state, track_incognito)?;
 
             let (title, url) = if let Some(browser_window_info) = browser_window_info {
                 if browser_window_info.is_incognito && !track_incognito {
