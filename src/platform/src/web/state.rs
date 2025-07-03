89e9854e88bf73c216f1b37e78a25af786be9aa2
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-07-03 22:45:22 +0800

    feat(platform): expand browser window state to its own struct that stores incognito info

--- src/platform/src/web/state.rs
+++ src/platform/src/web/state.rs
@@ -5,12 +5,21 @@ use util::future::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 use crate::objects::{ProcessId, Window};
 
+/// Information about a browser window
+#[derive(Debug, Default, Clone)]
+pub struct BrowserWindowInfo {
+    /// Whether the browser window is in incognito mode
+    pub is_incognito: bool,
+}
+
 /// Shared inner state of browsers and websites seen in the desktop
 #[derive(Debug, Default)]
 pub struct StateInner {
-    /// Cache of whether a window is a browser or not.
+    /// Cache of whether a browser window states.
+    /// If present and None, this is not a browser window.
+    /// If present and Some, this is a browser window.
     /// Not present means that we don't know if it's a browser or not.
-    pub browser_windows: SmallHashMap<Window, bool>,
+    pub browser_windows: SmallHashMap<Window, Option<BrowserWindowInfo>>,
     /// Processes that are known to be browsers.
     pub browser_processes: SmallHashSet<ProcessId>,
 }
