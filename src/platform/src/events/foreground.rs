92f4c11e9e13124baa6c6cc95ce623fc88af8718
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-07-13 19:51:51 +0800

    refactor(engine): use window instead of fg

--- src/platform/src/events/foreground.rs
+++ src/platform/src/events/foreground.rs
@@ -52,18 +52,18 @@ impl ForegroundEventWatcher {
         mut web_state: WriteLockedState<'_>,
         track_incognito: bool,
     ) -> Result<Option<WindowSession>> {
-        if let Some(fg) = Window::foreground() {
+        if let Some(window) = Window::foreground() {
             let (is_browser, fetched_path) = {
-                let is_browser = web_state.browser_windows.get(&fg).cloned();
+                let is_browser = web_state.browser_windows.get(&window).cloned();
                 if let Some(is_browser) = is_browser {
                     // We know already if it's a browser or not
                     (is_browser, None)
                 } else {
                     // Educated guess. Note that VSCode will pass this check since it's a chromium app.
-                    let maybe_browser = browser.is_maybe_chromium_window(&fg).warn();
+                    let maybe_browser = browser.is_maybe_chromium_window(&window).warn();
                     // So we further check if it's a chromium process
                     let (is_browser, fetched_path) = if maybe_browser {
-                        let ptid = fg.ptid().context("get ptid")?;
+                        let ptid = window.ptid().context("get ptid")?;
                         if web_state.browser_processes.contains(&ptid.pid) {
                             (true, None)
                         } else {
@@ -79,7 +79,7 @@ impl ForegroundEventWatcher {
                         (false, None)
                     };
 
-                    web_state.browser_windows.insert(fg.clone(), is_browser);
+                    web_state.browser_windows.insert(window.clone(), is_browser);
 
                     (is_browser, fetched_path)
                 }
@@ -87,7 +87,7 @@ impl ForegroundEventWatcher {
             drop(web_state);
 
             let (title, url) = if is_browser {
-                let element = browser.get_chromium_element(&fg)?;
+                let element = browser.get_chromium_element(&window)?;
                 let incognito = browser
                     .chromium_incognito(&element)
                     .context("get chromium incognito")?;
@@ -98,17 +98,17 @@ impl ForegroundEventWatcher {
                     ("<Incognito>".to_string(), None)
                 } else {
                     let url = browser.chromium_url(&element).context("get chromium url")?;
-                    let title = fg.title()?;
+                    let title = window.title()?;
                     (title, url)
                 }
             } else {
-                (fg.title()?, None)
+                (window.title()?, None)
             };
 
             let session = WindowSession {
                 title,
                 url,
-                window: fg,
+                window,
                 fetched_path,
             };
             return Ok(Some(session));
