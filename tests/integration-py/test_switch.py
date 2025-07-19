90bcb41aa90fa4d108e038045c2f59335c4d36c0
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-07-19 23:10:54 +0800

    feat(test): add test_switch permutations

--- tests/integration-py/test_switch.py
+++ tests/integration-py/test_switch.py
@@ -1,11 +1,13 @@
 import time
 import pytest
 import logging
+import itertools
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import pyautogui
 from driver import (
     driver_web_state,
     DriverData,
@@ -31,22 +33,46 @@ def browser():
     driver.quit()
 
 
-DELAY = 2
+urls = {
+    "https1": {
+        "url": "https://chromium.googlesource.com/chromium/src/+/refs/heads/lkgr/docs/design/README.md",
+        "title": "Chromium Docs - Chromium Design Docs",
+    },
+    "https2": {
+        "url": "https://learn.microsoft.com/en-us/windows/win32/winauto/microsoft-active-accessibility-and-ui-automation-compared",
+        "title": "Microsoft Active Accessibility and UI Automation Compared - Win32 apps | Microsoft Learn",
+    },
+    "chrome internal": {
+        "url": "chrome://flags/1",  # use /1 due to omnibox and document value differences
+        "title": "Experiments",
+    },
+}
 
 
+url_pairs = itertools.permutations(urls.keys(), 2)
+
+DELAY = 5
+
+
+@pytest.mark.parametrize("url1,url2", url_pairs)
 def test_switch(
-    driver_web_state: DriverData, browser: webdriver.Chrome, events: RecordedEvents
+    url1: str,
+    url2: str,
+    driver_web_state: DriverData,
+    browser: webdriver.Chrome,
+    events: RecordedEvents,
 ):
-    url1 = "https://chromium.googlesource.com/chromium/src/+/refs/heads/lkgr/docs/design/README.md"
-    url2 = "https://learn.microsoft.com/en-us/windows/win32/winauto/microsoft-active-accessibility-and-ui-automation-compared"
-    title1 = "Chromium Docs - Chromium Design Docs"
-    title2 = "Microsoft Active Accessibility and UI Automation Compared - Win32 apps | Microsoft Learn"
+    url1, title1 = urls[url1]["url"], urls[url1]["title"]
+    url2, title2 = urls[url2]["url"], urls[url2]["title"]
 
     # open url1 and url2 in new tabs
     logger.info(f"Opening first tab: {url1}")
     browser.get(url1)
     logger.info(f"Opening second tab: {url2}")
-    browser.execute_script(f"window.open('{url2}', '_blank');")
+    pyautogui.hotkey("ctrl", "t")
+    pyautogui.typewrite(url2)
+    time.sleep(0.1)
+    pyautogui.press("enter")
     WebDriverWait(browser, 10).until(
         EC.presence_of_element_located((By.TAG_NAME, "body"))
     )
