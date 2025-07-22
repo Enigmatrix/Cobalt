3198158ad233dc151bcff8148ec57af54384b077
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-07-23 07:13:07 +0800

    feat(platform): add property change handler for uiautomation

--- src/platform/src/events/mod.rs
+++ src/platform/src/events/mod.rs
@@ -1,9 +1,11 @@
 mod foreground;
 mod interaction;
 mod name;
+mod prop_change;
 mod system;
 
 pub use foreground::*;
 pub use interaction::*;
 pub use name::*;
+pub use prop_change::*;
 pub use system::*;
