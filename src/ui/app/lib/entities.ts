2b6f84fd8bbc9d5a40103dc26e73ba0ccdcce731
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-08-05 00:47:54 +0800

    fix(ui): inheritance in With*

--- src/ui/app/lib/entities.ts
+++ src/ui/app/lib/entities.ts
@@ -15,9 +15,7 @@ export interface WithDuration<T> {
   duration: Duration;
 }
 
-export interface WithGroupedDuration<T> {
-  id: Ref<T>;
-  duration: Duration;
+export interface WithGroupedDuration<T> extends WithDuration<T> {
   group: Timestamp;
 }
 
