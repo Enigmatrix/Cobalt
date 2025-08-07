453e693b123f72b09984a28294d8fa2ec4eccbcb
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2025-08-07 16:42:42 +0800

    feat(ui): show triggered alerts in dashboard

--- src/ui/app/routes/home.tsx
+++ src/ui/app/routes/home.tsx
@@ -1,3 +1,5 @@
+import AppIcon from "@/components/app/app-icon";
+import { ScoreCircle } from "@/components/tag/score";
 import { DateRangePicker } from "@/components/time/date-range-picker";
 import { DurationText } from "@/components/time/duration-text";
 import {
@@ -8,6 +10,13 @@ import {
 } from "@/components/ui/breadcrumb";
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Text } from "@/components/ui/text";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { NextButton, PrevButton, UsageCard } from "@/components/usage-card";
 import { Gantt } from "@/components/viz/gantt2";
 import { UsageChart } from "@/components/viz/usage-chart";
@@ -18,6 +27,7 @@ import {
   VizCardHeader,
   VizCardTitle,
 } from "@/components/viz/viz-card";
+import { useAlerts, useApp, useTag } from "@/hooks/use-refresh";
 import {
   useAppDurationsPerPeriod,
   useAppSessionUsages,
@@ -31,6 +41,7 @@ import {
   useIntervalControlsWithDefault,
   usePeriodInterval,
 } from "@/hooks/use-time";
+import type { Alert, TimeFrame } from "@/lib/entities";
 import {
   hour24Formatter,
   monthDayFormatter,
@@ -38,6 +49,8 @@ import {
   type Period,
 } from "@/lib/time";
 import { cn } from "@/lib/utils";
+import _ from "lodash";
+import { TagIcon } from "lucide-react";
 import { DateTime } from "luxon";
 import { useMemo } from "react";
 
@@ -68,7 +81,8 @@ export default function Home() {
       </header>
       <div className="h-0 flex-auto overflow-y-auto overflow-x-hidden [scrollbar-gutter:stable]">
         <div className="flex flex-col gap-4 p-4">
-          <div className="grid auto-rows-min gap-4 grid-cols-3">
+          <div className="grid auto-rows-min gap-4 grid-cols-4">
+            <AlertEventItem />
             <UsageStatItem
               label="Focus Streaks"
               usage={focusStreakUsage}
@@ -264,10 +278,114 @@ function UsageStatItem({
       <div className="flex flex-col">
         <div className={cn("text-sm font-medium", color)}>{label}</div>
         <DurationText
-          className={cn("text-xl font-bold", color)}
+          className={cn("text-xl font-bold self-end", color)}
           ticks={usage}
         />
       </div>
+    </div>
+  );
+}
+
+function AlertEventItem() {
+  const alerts = useAlerts();
+  const triggeredAlerts = useMemo(
+    () =>
+      _(alerts)
+        .filter((alert) => alert.events.today > 0)
+        .value(),
+    [alerts],
+  );
+
+  return (
+    <div className="rounded-lg p-4 shadow-sm border text-right bg-card border-border">
+      <div className="flex flex-col">
+        <div className="text-sm font-medium">Triggered Alerts</div>
+        {triggeredAlerts.length > 0 ? (
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger className="self-end">
+                <div className="text-xl font-bold">
+                  {triggeredAlerts.length}
+                </div>
+              </TooltipTrigger>
+              <TooltipContent className="gap-2">
+                {triggeredAlerts.map((alert) => (
+                  <MiniAlertItem key={alert.id} alert={alert} />
+                ))}
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        ) : (
+          <div className="text-xl font-bold">None</div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+function MiniAlertItem({ alert }: { alert: Alert }) {
+  return (
+    <div className="shadow-lg p-1 rounded-md bg-card border border-border flex flex-row gap-2 items-center">
+      <MiniTargetItem alert={alert} />
+      <div className="flex flex-row gap-1 items-center text-xs text-muted-foreground">
+        <DurationText ticks={alert.usageLimit} />
+        <div className="">{" / " + timeFrameToString(alert.timeFrame)}</div>
+      </div>
+      <MiniAlertTriggerAction alert={alert} />
+    </div>
+  );
+}
+
+function timeFrameToString(timeFrame: TimeFrame) {
+  return timeFrame === "daily"
+    ? "day"
+    : timeFrame === "weekly"
+      ? "week"
+      : "month";
+}
+
+function MiniTargetItem({ alert }: { alert: Alert }) {
+  const app = useApp(alert.target.tag === "app" ? alert.target.id : null);
+  const tag = useTag(alert.target.tag === "tag" ? alert.target.id : null);
+
+  return alert.target.tag === "app" && app ? (
+    <div className="flex gap-1 items-center">
+      <AppIcon className="h-4 w-4 shrink-0" appIcon={app.icon} />
+      <div>{app.name}</div>
+    </div>
+  ) : alert.target.tag === "tag" && tag ? (
+    <div className="flex gap-1 items-center">
+      <TagIcon className="h-4 w-4 shrink-0" style={{ color: tag.color }} />
+      <div>{tag.name}</div>
+      <ScoreCircle score={tag.score} />
+    </div>
+  ) : null;
+}
+
+function MiniAlertTriggerAction({ alert }: { alert: Alert }) {
+  return (
+    <div className="flex gap-1 items-center text-xs text-muted-foreground">
+      <span>
+        {alert.triggerAction.tag === "dim"
+          ? "Dim"
+          : alert.triggerAction.tag === "message"
+            ? "Message"
+            : "Kill"}
+      </span>
+      {alert.triggerAction.tag === "dim" && (
+        <div className="flex items-center">
+          <span>(</span>
+          <DurationText ticks={alert.triggerAction.duration} />
+          <span>)</span>
+        </div>
+      )}
+      {alert.triggerAction.tag === "message" && (
+        <div className="flex items-center">
+          <span>(</span>
+          <Text className="max-w-24">{alert.triggerAction.content}</Text>
+          <span>)</span>
+        </div>
+      )}
     </div>
   );
 }
