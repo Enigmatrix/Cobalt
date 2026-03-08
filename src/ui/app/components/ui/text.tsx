2dfd0756a93a196ffc80d098140b16bfb5f4dd35
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2026-03-08 20:56:52 +0800

    fix(ui): spread extra props to app iconand text

--- src/ui/app/components/ui/text.tsx
+++ src/ui/app/components/ui/text.tsx
@@ -1,15 +1,17 @@
 import { cn } from "@/lib/utils";
 import type { ClassValue } from "clsx";
+import type { ComponentPropsWithoutRef } from "react";
 
 export function Text({
   children,
   className,
+  ...rest
 }: {
   children: string;
   className?: ClassValue;
-}) {
+} & Omit<ComponentPropsWithoutRef<"div">, "children" | "className" | "title">) {
   return (
-    <div className={cn("truncate", className)} title={children}>
+    <div className={cn("truncate", className)} title={children} {...rest}>
       {children || <span className="opacity-50 font-mono">Empty</span>}
     </div>
   );
